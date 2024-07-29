dbdecf699daa2906afbf4cbd3e0c12290d001726
Steve Yen <steve.yen@gmail.com>
2024-07-29 12:09:36 -0700

    Split long lines.

--- rosetta/core/cmd/main.py
+++ rosetta/core/cmd/main.py
@@ -9,16 +9,20 @@ def main():
     pass
 
 @main.command()
-@click.option('-sm', '--sentence-models', multiple=True, default=['sentence-transformers/all-MiniLM-L12-v2'],
+@click.option('-sm', '--sentence-models',
+              multiple=True,
+              default=['sentence-transformers/all-MiniLM-L12-v2'],
               help='SBERT models to download ahead of time.')
-@click.option('-od', '--output-directory', default=DEFAULT_OUTPUT_DIRECTORY,
+@click.option('-od', '--output-directory',
+              default=DEFAULT_OUTPUT_DIRECTORY,
               help='Location of the output directory to initialize (for non-CB-backed agents).')
 @click.option('-hist', '--history-directory',
               default=str((pathlib.Path(DEFAULT_OUTPUT_DIRECTORY) / DEFAULT_HISTORY_DIRECTORY).absolute()),
               help='Place to store the agent action history (relative to the output_directory).')
 def init(sentence_models, output_directory, history_directory):
     """Initialize the runtime environment (e.g., install sentence_transformers models)."""
-    cmd_initialize_local(sentence_models=sentence_models, output_directory=output_directory,
+    cmd_initialize_local(sentence_models=sentence_models,
+                         output_directory=output_directory,
                          history_directory=history_directory)
 
 @main.command()
@@ -37,11 +41,14 @@ def clean(catalog_file, history_dir):
 @click.option('-cf', '--catalog-file',
               default=str((pathlib.Path(DEFAULT_OUTPUT_DIRECTORY) / DEFAULT_CATALOG_FILENAME).absolute()),
               help='Name of the tool catalog to-be-generated (relative to the output directory).')
-@click.option('-em', '--embedding-model', default='sentence-transformers/all-MiniLM-L12-v2',
+@click.option('-em', '--embedding-model',
+              default='sentence-transformers/all-MiniLM-L12-v2',
               help='Embedding model to use when building the tool catalog.')
 def index(tool_dirs, catalog_file, embedding_model):
-    """Walk one or more directories and build a tool catalog from Python tools and descriptor files (*.sqlpp and *.yaml)."""
-    cmd_index_local(tool_dirs=tool_dirs, catalog_file=catalog_file, embedding_model=embedding_model)
+    """Walk one or more directories and build a catalog from Python source and descriptor files (*.sqlpp and *.yaml)."""
+    cmd_index_local(tool_dirs=tool_dirs,
+                    catalog_file=catalog_file,
+                    embedding_model=embedding_model)
 
 @main.command()
 def version():
