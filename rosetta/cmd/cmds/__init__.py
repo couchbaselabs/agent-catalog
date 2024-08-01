1e99192e487465e35b9c75c057d7b320d4b6eabf
Steve Yen <steve.yen@gmail.com>
2024-08-01 16:13:42 -0700

    Shorter import idiom.

--- rosetta/cmd/cmds/__init__.py
+++ rosetta/cmd/cmds/__init__.py
@@ -1,28 +1,12 @@
 # Our commands...
-from .clean import (
-    cmd_clean
-)
-from .env import (
-    cmd_env
-)
-from .find import (
-    cmd_find
-)
-from .index import (
-    cmd_index
-)
-from .publish import (
-    cmd_publish
-)
-from .status import (
-    cmd_status
-)
-from .version import (
-    cmd_version
-)
-from .web import (
-    cmd_web
-)
+from .clean import cmd_clean
+from .env import cmd_env
+from .find import cmd_find
+from .index import cmd_index
+from .publish import cmd_publish
+from .status import cmd_status
+from .version import cmd_version
+from .web import cmd_web
 
 
 # ...and our defaults.
--- rosetta/cmd/main.py
+++ rosetta/cmd/main.py
@@ -124,7 +124,7 @@ def status(ctx):
 @click.pass_context
 def version(ctx):
     """Show the version of this tool."""
-    cmd_version(ctx)
+    cmd_version(ctx.obj)
 
 
 @main.command()
