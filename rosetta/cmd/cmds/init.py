e5f1c72adabe4887bbf18715ff51f5ec352b78e0
Tanvi Johari <tanvi.johari@couchbase.com>
2024-08-05 13:51:18 +0530

    removed init couchbase

--- rosetta/cmd/cmds/init.py
+++ rosetta/cmd/cmds/init.py
@@ -1,18 +1,20 @@
 import json
 import os
 
-import couchbase.auth
-
 import sentence_transformers
 
 from rosetta.core.catalog import CATALOG_SCHEMA_VERSION
-from rosetta.core.catalog.version import lib_version, lib_version_compare, catalog_schema_version_compare
+from rosetta.core.catalog.version import (
+    catalog_schema_version_compare,
+    lib_version,
+    lib_version_compare,
+)
 
 
 def init_local(ctx, embedding_model: str):
     # Init directories.
-    os.makedirs(ctx['catalog'], exist_ok=True)
-    os.makedirs(ctx['activity'], exist_ok=True)
+    os.makedirs(ctx["catalog"], exist_ok=True)
+    os.makedirs(ctx["activity"], exist_ok=True)
 
     # Download embedding model to be cached for later runtime usage.
     if embedding_model:
@@ -26,30 +28,28 @@ def init_local(ctx, embedding_model: str):
 
     meta = {
         # Version of the local catalog data.
-        'catalog_schema_version': CATALOG_SCHEMA_VERSION,
-
+        "catalog_schema_version": CATALOG_SCHEMA_VERSION,
         # Version of the SDK library / tool that last wrote the local catalog data.
-        'lib_version': lib_v,
-
-        'embedding_model': embedding_model
+        "lib_version": lib_v,
+        "embedding_model": embedding_model,
     }
 
-    meta_path = ctx['catalog'] + '/meta.json'
+    meta_path = ctx["catalog"] + "/meta.json"
 
     if os.path.exists(meta_path):
-        with open(meta_path, 'r') as f:
+        with open(meta_path, "r") as f:
             meta = json.load(f)
 
-    if catalog_schema_version_compare(meta['catalog_schema_version'], CATALOG_SCHEMA_VERSION) > 0:
+    if catalog_schema_version_compare(meta["catalog_schema_version"], CATALOG_SCHEMA_VERSION) > 0:
         # TODO: Perhaps we're too strict here and should allow micro versions that get ahead.
         raise ValueError("Version of local catalog's catalog_schema_version is ahead.")
 
-    if lib_version_compare(meta['lib_version'], lib_v) > 0:
+    if lib_version_compare(meta["lib_version"], lib_v) > 0:
         # TODO: Perhaps we're too strict here and should allow micro versions that get ahead.
         raise ValueError("Version of local catalog's lib_version is ahead.")
 
-    meta['catalog_schema_version'] = CATALOG_SCHEMA_VERSION
-    meta['lib_version'] = lib_v
+    meta["catalog_schema_version"] = CATALOG_SCHEMA_VERSION
+    meta["lib_version"] = lib_v
 
     if embedding_model:
         # TODO: There might be other embedding model related
@@ -58,22 +58,16 @@ def init_local(ctx, embedding_model: str):
         # The embedding model should be the same over the life
         # of the local catalog, so that all the vectors will
         # be in the same, common, comparable vector space.
-        meta_embedding_model = meta.get('embedding_model')
-        if meta_embedding_model and \
-           meta_embedding_model != embedding_model:
-            raise ValueError(f"""The embedding model in the local catalog is currently {meta_embedding_model}.
-                             Use the 'clean' command to start over with a new embedding model of {embedding_model}.""")
+        meta_embedding_model = meta.get("embedding_model")
+        if meta_embedding_model and meta_embedding_model != embedding_model:
+            raise ValueError(
+                f"""The embedding model in the local catalog is currently {meta_embedding_model}.
+                             Use the 'clean' command to start over with a new embedding model of {embedding_model}."""
+            )
 
-        meta['embedding_model'] = embedding_model
+        meta["embedding_model"] = embedding_model
 
-    with open(meta_path, 'w') as f:
+    with open(meta_path, "w") as f:
         json.dump(meta, f, sort_keys=True, indent=4)
 
     return meta
-
-
-def init_db(embedding_model: str,
-            conn_string: str,
-            authenticator: couchbase.auth.Authenticator, **_):
-    # TODO (GLENN): Add initialization steps to create CB collections here.
-    pass
