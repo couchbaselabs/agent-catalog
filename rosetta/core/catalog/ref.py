4c13238ad070ffa6c2f93fa8bb0445ffb470810d
Steve Yen <steve.yen@gmail.com>
2024-08-06 16:22:27 -0700

    Started an abstract CatalogRef class and 3 subclasses.
    This is related to refactoring the current provider
    code into two envisioned parts...
    
    A) CatalogRef -- initializing a concrete CatalogRef subclass (whether
    for a local catalog or for a DB scope or a composite catalog),
    updating that CatalogRef instance with newer data, and
    searching/find()'ing ToolDescriptors from a CatalogRef instance...
    are envisioned to be part of (or near) this newly introduced
    core/catalog/ref.py file.
    
    B) Processing a single ToolDescriptor (which may or may not have come
    from the results of a CatalogRef.search/find()) into a callable python
    function instance (which might in turn be then passed onwards to a
    langchain-or-equivalent framework) is a separate, orthogonal maneuver,
    which will likely stay in the core/tool/provider.py file or nearby.

---
+++ rosetta/core/catalog/ref.py
@@ -0,0 +1,70 @@
+import abc
+import json
+import pathlib
+import typing
+
+from ..tool.types.descriptor import ToolDescriptor
+
+from .descriptor import CatalogDescriptor
+
+
+class CatalogRef(abc.ABC):
+    """ An abstract interface for a catalog reference. """
+
+    def find(self, todo):
+        """ Returns the ToolDescriptors from the catalog that best match a query. """
+        pass
+
+
+class LocalCatalogRef(CatalogRef):
+    """ Represents a local catalog. """
+
+    catalog_path: pathlib.Path
+
+    catalog_descriptor: CatalogDescriptor
+
+    def load(self):
+        with self.catalog_path.open('r') as fp:
+            self.catalog_descriptor = json.load(fp)
+
+    def find(self, todo) -> list[ToolDescriptor]:
+        """ Returns the ToolDescriptors that best match a query. """
+        pass # TODO.
+
+    def updateFrom(self, source: typing.Self, repo):
+        # TODO.
+        pass
+
+
+class DBCatalogRef(CatalogRef):
+    """ Represents a catalog stored in a database. """
+
+    def find(self, todo) -> list[ToolDescriptor]:
+        """ Returns the ToolDescriptors that best match a query. """
+        pass # TODO: SQL++ and vector index searches likely are involved here.
+
+    def updateFrom(self, source: LocalCatalogRef, repo):
+        # TODO.
+        pass
+
+
+class ChainedCatalogRef(CatalogRef):
+    """ Represents a chain of catalogs, where items from
+        earlier catalogs take precendence. """
+
+    children: list[CatalogRef]
+
+    def find(self, todo) -> list[ToolDescriptor]:
+        # TODO: This might roughly look something like...
+
+        results = []
+
+        for c in self.children:
+            results_c = c.find(todo)
+
+            # TODO: Filter out entries from results_c which
+            # are shadowed by the entries in the existing results.
+
+            results += results_c
+
+        return results
