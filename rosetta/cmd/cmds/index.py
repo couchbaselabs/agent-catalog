1e6b32cab7c173278099d9c4dd1ed6ad91c464b9
Steve Yen <steve.yen@gmail.com>
2024-08-05 19:27:17 -0700

    Refactor cmds/index.py to almost invoke the refactored indexers.

--- rosetta/cmd/cmds/index.py
+++ rosetta/cmd/cmds/index.py
@@ -1,27 +1,24 @@
+import fnmatch
 import pathlib
 
 import git
 
+from tqdm import tqdm
+
 from rosetta.core.catalog.dir import dir_scan
+
+from rosetta.core.tool.indexer import source_indexers
 
 from rosetta.cmd.cmds.init import init_local
 
 
-# TODO: This should be defined in rosetta.core somewhere.
-source_kinds = {
-    '*.py': {},
-    '*.sqlpp': {},
-    '*.rosetta-yaml': {}
-}
-
-# TODO: This should be defined in rosetta.core somewhere.
-source_kind_patterns = list(source_kinds.keys())
+source_globs = list(source_indexers.keys())
 
 
 # TODO: During index'ing, should we also record the source_dirs into the catalog?
-# TODO: Need to handle different kinds, e.g., option --kind="tool" or --kind="prompt", etc.
-# TODO: Or, can we avoid having the user / app-developer needing to provide a --kind option
-#       and instead just index all the different kinds?
+
+
+MAX_ERRS = 10 # TODO: Hardcoded limit on too many errors.
 
 
 def cmd_index(ctx, source_dirs: list[str], embedding_model: str, **_):
@@ -51,12 +48,73 @@ def cmd_index(ctx, source_dirs: list[str], embedding_model: str, **_):
 
     source_files = []
     for d in source_dirs:
-        source_files = source_files + dir_scan(d, source_kind_patterns)
+        source_files = source_files + dir_scan(d, source_globs)
 
-    for x in source_files:
-        print("Candidate:", x)
+    all_errs = []
+    all_descriptors = []
+
+    for source_file in tqdm(source_files):
+        if len(all_errs) > MAX_ERRS:
+            break
+
+        import time
+        time.sleep(0.1)
+
+        p = pathlib.Path(source_file)
+
+        def rev_ident_fn(filename: pathlib.Path) -> str:
+            print("rev_ident_fn", filename)
+            return "TODO-rev-ident" # TODO: Call repo GitPython API to retrieve commit SHA.
+
+        for glob, indexer in source_indexers.items():
+            if fnmatch.fnmatch(p.name, glob):
+                errs, descriptors = None, []
+
+                # TODO: This produces a pydantic error, like...
+                #   ERROR: 1 validation error for SemanticSearchMetadata input
+                #   Input should be a valid dictionary [type=dict_type,
+                #     input_value='{\n  "type": "object",\n...ing" }\n    }\n  }\n}\n', input_type=str]
+                #   For further information visit https://errors.pydantic.dev/2.8/v/dict_type
+                #
+                # errs, descriptors = indexer.start_descriptors(p, rev_ident_fn)
+
+                all_errs += errs or []
+                all_descriptors += [(descriptor, indexer) for descriptor in descriptors]
+
+                break
+
+    if not all_errs:
+        print("==================\naugmenting...")
+
+        for descriptor, indexer in tqdm(all_descriptors):
+            if len(all_errs) > MAX_ERRS:
+                break
+
+            all_errs += indexer.augment_descriptor(descriptor) or []
+
+    if not all_errs:
+        print("==================\nvectorizing...")
+
+        for descriptor, indexer in tqdm(all_descriptors):
+            if len(all_errs) > MAX_ERRS:
+                break
+
+            all_errs += indexer.vectorize_descriptor(descriptor) or []
+
+    if all_errs:
+        print("ERROR:", "\n".join([str(e) for e in all_errs]))
+
+        raise all_errs[0]
+
+    print("==================\nsaving local catalog...")
+
+    print("\n".join([descriptor for descriptor, indexer in descriptors]))
+
+    # TODO: Actually save the local catalog.
 
     # ---------------------------------
+
+    # TODO: Old indexing codepaths that are getting refactored.
 
     tool_catalog_file = ctx['catalog'] + '/tool_catalog.json'
 
