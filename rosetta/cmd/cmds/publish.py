e22616b5119a62aff0c6d1ebe3edb8cc634f396a
Tanvi Johari <tanvi.johari@couchbase.com>
2024-08-19 15:15:33 +0530

    all option for kind inpublish works
    Cleaning up
    
    Minor refactoring

--- rosetta/cmd/cmds/publish.py
+++ rosetta/cmd/cmds/publish.py
@@ -12,40 +12,36 @@ from rosetta.utils.publish import (
 from ..models import (
     Keyspace, Context
 )
-from ..defaults import (
-    DEFAULT_META_CATALOG_NAME
-)
 
 logger = logging.getLogger(__name__)
 
 
 # TODO (GLENN): I haven't tested these changes, but this signals a move towards a "version" object instead of a string.
 # TODO (GLENN): Use click.echo instead of print, and make use of the logger.
-def cmd_publish(ctx: Context, cluster, keyspace: Keyspace):
+
+def cmd_publish(ctx: Context, kind, cluster, keyspace: Keyspace):
+    if kind == "all":
+        kind_list = ["tool", "prompt"]
+        print("Inserting all catalogs...")
+    else:
+        kind_list = [kind]
+
     bucket = keyspace.bucket
     scope = keyspace.scope
-    catalog_file_name = ctx.catalog
-
-    folder_path = Path("./" + catalog_file_name)
-    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
     # Get bucket ref
     cb = cluster.bucket(bucket)
 
-    # Get the bucket manager
-    bucket_manager = cb.collections()
+    for kind in kind_list:
 
-    # Iterate over all catalog files
-    for col_type in files:
-        if str(col_type) == DEFAULT_META_CATALOG_NAME:
-            continue
+        catalog_path = Path(ctx.catalog) / (kind + "-catalog.json")
+        catalog = CatalogMem.load(catalog_path).catalog_descriptor
 
-        # Get catalog file
-        f = open("./" + catalog_file_name + "/" + col_type)
-        data: dict = json.load(f)
+        # Get the bucket manager
+        bucket_manager = cb.collections()
 
         # ----------Metadata collection----------
-        meta_col = col_type.split("-")[0] + "_metadata"
+        meta_col = kind + "_metadata"
         (msg, err) = create_scope_and_collection(bucket_manager, scope=scope, collection=meta_col)
         if err is not None:
             print(msg, err)
@@ -55,22 +51,22 @@ def cmd_publish(ctx: Context, cluster, keyspace: Keyspace):
         cb_coll = cb.scope(scope).collection(meta_col)
 
         # dict to store all the metadata - snapshot related data
-        metadata = {k: v for k, v in data.items() if k != 'items'}
+        metadata = {el: catalog.model_dump()[el] for el in catalog.model_dump() if el != 'items'}
 
         print("Upserting metadata..")
         try:
             key = metadata['version']['identifier']
             cb_coll.upsert(key, metadata)
             # print("Snapshot ",result.key," added to keyspace")
+        # TODO (GLENN): Should use the specific exception here instead of 'Exception'.
         except Exception as e:
             print("could not insert: ", e)
             return e
+        print("Metadata added!\n")
 
         # ----------Catalog items collection----------
-        catalog_col = col_type.split("-")[0] + "_catalog"
-        (msg, err) = create_scope_and_collection(
-            bucket_manager, scope=scope, collection=catalog_col
-        )
+        catalog_col = kind + "_catalog"
+        (msg, err) = create_scope_and_collection(bucket_manager, scope=scope, collection=catalog_col)
         if err is not None:
             print(msg, err)
             return
@@ -80,95 +76,24 @@ def cmd_publish(ctx: Context, cluster, keyspace: Keyspace):
 
         print("Upserting catalog items..")
 
-        for item in data["items"]:
+        # iterate over individual catalog items
+        for item in catalog.items:
             try:
-                key = item["identifier"]
-                item.update({"catalog_identifier": metadata['version']['identifier']})
-                cb_coll.upsert(key, item)
-                # print("Snapshot ",result.key," added to keyspace")
-            # TODO (GLENN): Should use the specific exception here instead of 'Exception'.
+                key = item.identifier
+
+                # serialise object to str
+                item = json.dumps(item.model_dump(), cls=CustomPublishEncoder)
+
+                # convert to dict object and insert snapshot id
+                item_json: dict = json.loads(item)
+                item_json.update({"catalog_identifier": metadata["version"]["identifier"]})
+
+                # upsert docs to CB collection
+                cb_coll.upsert(key, item_json)
             except Exception as e:
                 print("could not insert: ", e)
                 return e
 
-        f.close()
-        print("Inserted", col_type.split("-")[0], "catalog successfully!\n")
-
-    return "Successfully inserted all catalogs!"
-
-
-def cmd_publish_obj(ctx: Context, kind, cluster, keyspace: Keyspace):
-    if kind == "all":  # TODO: handle case later
-        print("all catalogs")
-        return
-
-    catalog_path = Path(ctx.catalog) / (kind + "-catalog.json")
-    catalog = CatalogMem.load(catalog_path).catalog_descriptor
-
-    bucket = keyspace.bucket
-    scope = keyspace.scope
-    # catalog_file_name = ctx.catalog
-
-    # Get bucket ref
-    cb = cluster.bucket(bucket)
-
-    # Get the bucket manager
-    bucket_manager = cb.collections()
-
-    # ----------Metadata collection----------
-    meta_col = kind + "_metadata"
-    (msg, err) = create_scope_and_collection(bucket_manager, scope=scope, collection=meta_col)
-    if err is not None:
-        print(msg, err)
-        return
-
-    # get collection ref
-    cb_coll = cb.scope(scope).collection(meta_col)
-
-    # dict to store all the metadata - snapshot related data
-    metadata = {k: v for k, v in catalog.model_dump() if k != 'items'}
-
-    print("Upserting metadata..")
-    try:
-        key = metadata['version']['identifier']
-        cb_coll.upsert(key, metadata)
-        # print("Snapshot ",result.key," added to keyspace")
-    # TODO (GLENN): Should use the specific exception here instead of 'Exception'.
-    except Exception as e:
-        print("could not insert: ", e)
-        return e
-    print("Metadata added!\n")
-
-    # ----------Catalog items collection----------
-    catalog_col = kind + "_catalog"
-    (msg, err) = create_scope_and_collection(bucket_manager, scope=scope, collection=catalog_col)
-    if err is not None:
-        print(msg, err)
-        return
-
-    # get collection ref
-    cb_coll = cb.scope(scope).collection(catalog_col)
-
-    print("Upserting catalog items..")
-
-    # iterate over individual catalog items
-    for item in catalog.items:
-        try:
-            key = item.identifier
-
-            # serialise object to str
-            item = json.dumps(item.model_dump(), cls=CustomPublishEncoder)
-
-            # convert to dict object and insert snapshot id
-            item_json: dict = json.loads(item)
-            item_json.update({"catalog_identifier": metadata["version"]["identifier"]})
-
-            # upsert docs to CB collection
-            cb_coll.upsert(key, item_json)
-        except Exception as e:
-            print("could not insert: ", e)
-            return e
-
-    print("Inserted", kind, "catalog successfully!\n")
+        print("Inserted", kind, "catalog successfully!\n")
 
     return "Successfully inserted all catalogs!"
--- rosetta/cmd/main.py
+++ rosetta/cmd/main.py
@@ -22,7 +22,7 @@ from ..cmd.models.publish import Keyspace, CouchbaseConnect
 from ..cmd.models.context import Context
 
 from .cmds import *
-from .cmds.publish import cmd_publish, cmd_publish_obj
+from .cmds.publish import cmd_publish
 from .defaults import *
 
 # TODO: Should we load from ".env.rosetta"?
@@ -193,61 +193,19 @@ def index(ctx, source_dirs, kind, embedding_model, include_dirty, dry_run):
 
 @click_main.command()
 @click.option(
+    "--kind",
+    default="all",
+    help="The kind of catalog to insert into DB.",
+    show_default=True,
+)
+@click.option(
     "-sc",
     "--scope",
     default="rosetta-catalog",
     help="Couchbase Scope where data is inserted.",
 )
 @click.pass_context
-def publish(ctx, scope):
-    """ Publish the local catalog to a Couchbase instance. """
-
-    keyspace_details = Keyspace(bucket="", scope=scope)
-    connection_details = CouchbaseConnect(
-        connection_url=os.getenv("CB_CONN_STRING"),
-        username=os.getenv("CB_USERNAME"),
-        password=os.getenv("CB_PASSWORD"),
-    )
-
-    # Establish a connection
-    err, cluster = get_connection(conn=connection_details)
-    if err:
-        click.echo(str(err))
-        return
-
-    # Get buckets from CB Cluster
-    buckets = get_buckets(cluster=cluster)
-
-    # Prompt user to select a bucket
-    selected_bucket = click.prompt(
-        "Please select a bucket", type=click.Choice(buckets), show_choices=True
-    )
-    click.echo(f"Inserting documents in : {selected_bucket}/{keyspace_details.scope}\n")
-    keyspace_details.bucket = selected_bucket
-
-    # Publish catalog into keyspace
-    msg = cmd_publish(ctx.obj, cluster=cluster, keyspace=keyspace_details)
-    print(msg)
-
-    # Close cluster connection
-    cluster.close()
-
-
-@click_main.command()
-@click.option(
-    "--kind",
-    default="tool",
-    help="The kind of catalog to show status.",
-    show_default=True,
-)
-@click.option(
-    "-sc",
-    "--scope",
-    default="rosetta-catalog-publish-serialise",
-    help="Couchbase Scope where data is inserted.",
-)
-@click.pass_context
-def publishobj(ctx, kind, scope):
+def publish(ctx, kind, scope):
     """Publish command that inserts after reading CatalogMem object"""
     keyspace_details = Keyspace(bucket="", scope=scope)
     connection_details = CouchbaseConnect(
@@ -271,7 +229,7 @@ def publishobj(ctx, kind, scope):
     )
     click.echo(f"Inserting documents in : {selected_bucket}/{keyspace_details.scope}\n")
     keyspace_details.bucket = selected_bucket
-    cmd_publish_obj(ctx.obj, kind, cluster, keyspace_details)
+    cmd_publish(ctx.obj, kind, cluster, keyspace_details)
 
     cluster.close()
 
