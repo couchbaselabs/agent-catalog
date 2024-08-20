a2a469d4b390449676323e49039f2a83d8f8f93b
Tanvi Johari <tanvi.johari@couchbase.com>
2024-08-20 15:59:38 +0530

    Added annotations to publish

--- rosetta_cmd/cmds/publish.py
+++ rosetta_cmd/cmds/publish.py
@@ -3,14 +3,9 @@ import logging
 from pathlib import Path
 
 from rosetta_core.catalog.catalog_mem import CatalogMem
-from rosetta_util.publish import (
-    create_scope_and_collection,
-    CustomPublishEncoder
-)
+from rosetta_util.publish import create_scope_and_collection, CustomPublishEncoder
 
-from ..models import (
-    Keyspace, Context
-)
+from ..models import Keyspace, Context
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +13,8 @@ logger = logging.getLogger(__name__)
 # TODO (GLENN): I haven't tested these changes, but this signals a move towards a "version" object instead of a string.
 # TODO (GLENN): Use click.echo instead of print, and make use of the logger.
 
-def cmd_publish(ctx: Context, kind, cluster, keyspace: Keyspace, printer):
+
+def cmd_publish(ctx: Context, kind, annotations: list[dict], cluster, keyspace: Keyspace, printer):
     if kind == "all":
         kind_list = ["tool", "prompt"]
         logger.info("Inserting all catalogs...")
@@ -50,11 +46,17 @@ def cmd_publish(ctx: Context, kind, cluster, keyspace: Keyspace, printer):
         cb_coll = cb.scope(scope).collection(meta_col)
 
         # dict to store all the metadata - snapshot related data
-        metadata = {el: catalog.model_dump()[el] for el in catalog.model_dump() if el != 'items'}
+        metadata = {el: catalog.model_dump()[el] for el in catalog.model_dump() if el != "items"}
+
+        # add annotations to metadata
+        annotations_list = {
+            an[0]: an[1].split("+") if "+" in an[1] else an[1] for an in annotations
+        }
+        metadata.update({"snapshot_annotations": annotations_list})
 
         printer("Upserting metadata..")
         try:
-            key = metadata['version']['identifier']
+            key = metadata["version"]["identifier"]
             cb_coll.upsert(key, metadata)
             # printer("Snapshot ",result.key," added to keyspace")
         # TODO (GLENN): Should use the specific exception here instead of 'Exception'.
@@ -65,7 +67,9 @@ def cmd_publish(ctx: Context, kind, cluster, keyspace: Keyspace, printer):
 
         # ----------Catalog items collection----------
         catalog_col = kind + "_catalog"
-        (msg, err) = create_scope_and_collection(bucket_manager, scope=scope, collection=catalog_col)
+        (msg, err) = create_scope_and_collection(
+            bucket_manager, scope=scope, collection=catalog_col
+        )
         if err is not None:
             printer(msg, err)
             return
@@ -86,6 +90,7 @@ def cmd_publish(ctx: Context, kind, cluster, keyspace: Keyspace, printer):
                 # convert to dict object and insert snapshot id
                 item_json: dict = json.loads(item)
                 item_json.update({"catalog_identifier": metadata["version"]["identifier"]})
+                item_json.update({"snapshot_annotations": annotations_list})
 
                 # upsert docs to CB collection
                 cb_coll.upsert(key, item_json)
