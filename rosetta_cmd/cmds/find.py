0b7f20aeb335ee6027e8c527af5d3504471e74ce
Tanvi Johari <tanvi.johari@couchbase.com>
2024-08-26 16:33:18 +0530

    FindDB: added --search-db option, track and create vector indexes

--- rosetta_cmd/cmds/find.py
+++ rosetta_cmd/cmds/find.py
@@ -7,11 +7,13 @@ import tqdm
 
 from ..defaults import DEFAULT_MAX_ERRS
 from ..defaults import DEFAULT_SCAN_DIRECTORY_OPTS
-from ..models.context import Context
+from ..models import Context
+from ..models import CouchbaseConnect
 from .util import init_local
 from .util import load_repository
 from rosetta_core.annotation import AnnotationPredicate
 from rosetta_core.catalog.catalog_base import SearchResult
+from rosetta_core.catalog.catalog_db import CatalogDB
 from rosetta_core.catalog.catalog_mem import CatalogMem
 from rosetta_core.catalog.index import index_catalog
 from rosetta_core.provider.refiner import ClosestClusterRefiner
@@ -26,7 +28,18 @@ refiners = {
 logger = logging.getLogger(__name__)
 
 
-def cmd_find(ctx: Context, query, kind="tool", limit=1, include_dirty=True, refiner=None, annotations=None):
+def cmd_find(
+    ctx: Context,
+    query,
+    bucket,
+    kind="tool",
+    limit=1,
+    include_dirty=True,
+    refiner=None,
+    annotations=None,
+    search_db=False,
+    conn: CouchbaseConnect = None,
+):
     # TODO: One day, also handle DBCatalogRef?
     # TODO: If DB is outdated and the local catalog has newer info,
     #       then we need to consult the latest, local catalog / MemCatalogRef?
@@ -41,42 +54,56 @@ def cmd_find(ctx: Context, query, kind="tool", limit=1, include_dirty=True, refi
         valid_refiners.sort()
         raise ValueError(f"ERROR: unknown refiner, valid refiners: {valid_refiners}")
 
-    catalog_path = pathlib.Path(ctx.catalog) / (kind + "-catalog.json")
+    if search_db:  # WIP
+        catalog = CatalogDB()
+        print("searching db...")
 
-    catalog = CatalogMem().load(catalog_path)
+        annotations_predicate = AnnotationPredicate(annotations) if annotations is not None else None
 
-    if include_dirty:
-        repo, get_path_version = load_repository(pathlib.Path(os.getcwd()))
-        if repo and repo.is_dirty():
-            meta = init_local(ctx, catalog.catalog_descriptor.embedding_model, read_only=True)
-
-            # The repo and any dirty files do not have real commit id's, so use "DIRTY".
-            version = VersionDescriptor(is_dirty=True)
-
-            # Scan the same source_dirs that were used in the last "rosetta index".
-            source_dirs = catalog.catalog_descriptor.source_dirs
-
-            # Create a CatalogMem on-the-fly that incorporates the dirty
-            # source file items which we'll use instead of the local catalog file.
-            catalog = index_catalog(
-                meta,
-                version,
-                get_path_version,
-                kind,
-                catalog_path,
-                source_dirs,
-                scan_directory_opts=DEFAULT_SCAN_DIRECTORY_OPTS,
-                printer=logger.debug if not ctx.verbose else click.echo,
-                progress=(lambda a: a) if not ctx.verbose else tqdm.tqdm,
-                max_errs=DEFAULT_MAX_ERRS,
+        search_results = [
+            SearchResult(entry=x.entry, delta=x.delta)
+            for x in catalog.find(
+                query, limit=limit, annotations=annotations_predicate, bucket=bucket, kind=kind, conn=conn
             )
+        ]
+    else:
+        catalog_path = pathlib.Path(ctx.catalog) / (kind + "-catalog.json")
 
-    # Query the catalog for a list of results.
-    annotations_predicate = AnnotationPredicate(annotations) if annotations is not None else None
-    search_results = [
-        SearchResult(entry=x.entry, delta=x.delta)
-        for x in catalog.find(query, limit=limit, annotations=annotations_predicate)
-    ]
+        catalog = CatalogMem().load(catalog_path)
+
+        if include_dirty:
+            repo, get_path_version = load_repository(pathlib.Path(os.getcwd()))
+            if repo and repo.is_dirty():
+                meta = init_local(ctx, catalog.catalog_descriptor.embedding_model, read_only=True)
+
+                # The repo and any dirty files do not have real commit id's, so use "DIRTY".
+                version = VersionDescriptor(is_dirty=True)
+
+                # Scan the same source_dirs that were used in the last "rosetta index".
+                source_dirs = catalog.catalog_descriptor.source_dirs
+
+                # Create a CatalogMem on-the-fly that incorporates the dirty
+                # source file items which we'll use instead of the local catalog file.
+                catalog = index_catalog(
+                    meta,
+                    version,
+                    get_path_version,
+                    kind,
+                    catalog_path,
+                    source_dirs,
+                    scan_directory_opts=DEFAULT_SCAN_DIRECTORY_OPTS,
+                    printer=logger.debug if not ctx.verbose else click.echo,
+                    progress=(lambda a: a) if not ctx.verbose else tqdm.tqdm,
+                    max_errs=DEFAULT_MAX_ERRS,
+                )
+
+        # Query the catalog for a list of results.
+        annotations_predicate = AnnotationPredicate(annotations) if annotations is not None else None
+        search_results = [
+            SearchResult(entry=x.entry, delta=x.delta)
+            for x in catalog.find(query, limit=limit, annotations=annotations_predicate)
+        ]
+
     if refiner is not None:
         search_results = refiners[refiner]()(search_results)
     click.secho(f"{len(search_results)} result(s) returned from the catalog.", bold=True, bg="green")
--- rosetta_core/catalog/catalog_db.py
+++ rosetta_core/catalog/catalog_db.py
@@ -1,18 +1,137 @@
+import click
+import logging
+import pickle
 import typing
 
 from ..annotation import AnnotationPredicate
+from ..catalog.descriptor import CatalogDescriptor
 from .catalog_base import CatalogBase
 from .catalog_base import SearchResult
+from rosetta_cmd.models import CouchbaseConnect
+
+logger = logging.getLogger(__name__)
 
 
 class CatalogDB(CatalogBase):
     """Represents a catalog stored in a database."""
 
     # TODO: This probably has fields of conn info, etc.
+    catalog_descriptor: CatalogDescriptor = None
+
+    @classmethod
+    def create_vector_index(
+        cls, bucket: str = "", kind: str = "tool", conn: CouchbaseConnect = ""
+    ) -> tuple[str | None, Exception | None]:
+        import json
+        import requests
+
+        index_to_create = f"{bucket}.rosetta-catalog.rosetta-{kind}-index"
+        try:
+            with open("created_indexes.p", "rb") as file:
+                created_indexes_set = pickle.load(file)
+        except pickle.UnpicklingError:
+            print("Error unpickling the file. The file may be corrupted or not a valid pickle file.")
+
+        if index_to_create not in created_indexes_set:
+            create_vector_index_url = (
+                f"http://localhost:8094/api/bucket/{bucket}/scope/rosetta-catalog/index/rosetta-{kind}-index"
+            )
+            headers = {
+                "Content-Type": "application/json",
+            }
+            auth = (conn.username, conn.password)
+
+            payload = json.dumps(
+                {
+                    "type": "fulltext-index",
+                    "name": f"{bucket}.rosetta-catalog.rosetta-{kind}-vec",
+                    "sourceType": "gocbcore",
+                    "sourceName": f"{bucket}",
+                    "planParams": {"maxPartitionsPerPIndex": 1024, "indexPartitions": 1},
+                    "params": {
+                        "doc_config": {
+                            "docid_prefix_delim": "",
+                            "docid_regexp": "",
+                            "mode": "scope.collection.type_field",
+                            "type_field": "type",
+                        },
+                        "mapping": {
+                            "analysis": {},
+                            "default_analyzer": "standard",
+                            "default_datetime_parser": "dateTimeOptional",
+                            "default_field": "_all",
+                            "default_mapping": {"dynamic": True, "enabled": False},
+                            "default_type": "_default",
+                            "docvalues_dynamic": False,
+                            "index_dynamic": True,
+                            "store_dynamic": False,
+                            "type_field": "_type",
+                            "types": {
+                                f"rosetta-catalog.{kind}_catalog": {
+                                    "dynamic": False,
+                                    "enabled": True,
+                                    "properties": {
+                                        "embedding": {
+                                            "dynamic": False,
+                                            "enabled": True,
+                                            "fields": [{"index": True, "name": "embedding", "type": "text"}],
+                                        }
+                                    },
+                                }
+                            },
+                        },
+                        "store": {"indexType": "scorch", "segmentVersion": 15},
+                    },
+                    "sourceParams": {},
+                }
+            )
+
+            try:
+                response = requests.request("PUT", create_vector_index_url, headers=headers, auth=auth, data=payload)
+
+                if json.loads(response.text)["status"] == "ok":
+                    # Add created index name to global set
+                    created_indexes_set.add(index_to_create)
+                    logger.info("add to created indexes ", created_indexes_set)
+                    try:
+                        with open("created_indexes.p", "wb") as file:
+                            pickle.dump(created_indexes_set, file)
+                    except pickle.PicklingError:
+                        print("Error pickling the data. The data may not be serializable.")
+
+                    return index_to_create, None
+                elif json.loads(response.text)["status"] == "fail":
+                    raise Exception(json.loads(response.text)["error"])
+            except Exception as e:
+                return None, e
+        else:
+            return index_to_create, None
 
     def find(
-        self, query: str, limit: typing.Union[int | None] = 1, annotations: AnnotationPredicate = None
+        self,
+        query: str,
+        limit: typing.Union[int | None] = 1,
+        annotations: AnnotationPredicate = None,
+        bucket: str = "",
+        kind: str = "tool",
+        conn: CouchbaseConnect = "",
     ) -> list[SearchResult]:
         """Returns the catalog items that best match a query."""
 
-        return []  # TODO: SQL++ and vector index searches likely are involved here.
+        # TODO: If annotations have been specified, prune all tools that do not possess these annotations.
+
+        # Create a vector index for the kind, if it does not exist
+        index, err = CatalogDB.create_vector_index(bucket, kind, conn)
+        if err is not None:
+            click.secho(f"Error creating index: {err}", fg="red")
+        else:
+            logger.info(f"Index to use: {index}")
+
+        # TODO: Perform semantic search
+
+        # TODO: Order results
+
+        # TODO: Apply our limit clause.
+        # if limit > 0:
+        #     results = results[:limit]
+        # return results
