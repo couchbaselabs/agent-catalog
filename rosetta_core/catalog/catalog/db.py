6356c67e66927fd7d5103492dd931f537106595c
Tanvi Johari <tanvi.johari@couchbase.com>
2024-08-29 18:56:58 +0530

    Added db find method that executed a query for search and filter

--- rosetta_core/catalog/catalog/db.py
+++ rosetta_core/catalog/catalog/db.py
@@ -2,6 +2,11 @@ import click
 import logging
 import typing
 
+from ...record.descriptor import RecordDescriptor
+from ...tool.descriptor import HTTPRequestToolDescriptor
+from ...tool.descriptor import PythonToolDescriptor
+from ...tool.descriptor import SemanticSearchToolDescriptor
+from ...tool.descriptor import SQLPPQueryToolDescriptor
 from .base import CatalogBase
 from .base import SearchResult
 from rosetta_cmd.models import Keyspace
@@ -11,11 +16,8 @@ from rosetta_util.query import execute_query
 logger = logging.getLogger(__name__)
 
 
-# noinspection SqlNoDataSourceInspection
 class CatalogDB(CatalogBase):
     """Represents a catalog stored in a database."""
-
-    # TODO: This probably has fields of conn info, etc.
 
     def find(
         self,
@@ -27,39 +29,75 @@ class CatalogDB(CatalogBase):
         snapshot_id: typing.Union[str | None] = "all",
         cluster: any = "",
         keyspace: Keyspace = None,
+        meta: any = None,
     ) -> list[SearchResult]:
         """Returns the catalog items that best match a query."""
 
-        # Get all items from catalog
+        # Generate embeddings for user query
+        import sentence_transformers
+
+        embedding_model_obj = sentence_transformers.SentenceTransformer(
+            meta["embedding_model"], tokenizer_kwargs={"clean_up_tokenization_spaces": True}
+        )
+        query_embeddings = embedding_model_obj.encode(query).tolist()
+
+        # Get all relevant items from catalog
+        # TODO: check if annotations can be added in the query itself -> not a good idea to do this in query
+        # TODO: get delta (score) from SEARCH func
+
+        # User has specified a snapshot id
         if snapshot_id != "all":
             filter_records_query = (
-                f"SELECT * FROM `{bucket}`.`rosetta-catalog`.`{kind}_catalog`",
-                f'WHERE catalog_identifier="{snapshot_id}";',
+                f"SELECT t.* FROM `{bucket}`.`rosetta-catalog`.`{kind}_catalog` as t "
+                + f"WHERE catalog_identifier='{snapshot_id}' AND "
+                + "SEARCH(t, {"
+                + f"'query': '{query}',"
+                + "'knn': {'field': 'embedding',"
+                + f"'value': '[{query_embeddings}]',"
+                + "'k': 10"
+                + "}, 'size': 10, 'ctl': { 'timeout': 10 } }) "
+                + f"LIMIT {limit};"
             )
+        # No snapshot id has been mentioned
         else:
-            filter_records_query = f"SELECT * FROM `{bucket}`.`rosetta-catalog`.`{kind}_catalog`;"
+            filter_records_query = (
+                f"SELECT t.* FROM `{bucket}`.`rosetta-catalog`.`{kind}_catalog` as t "
+                + "WHERE SEARCH(t, {"
+                + f"'query': '{query}',"
+                + "'knn': {'field': 'embedding',"
+                + f"'value': '[{query_embeddings}]',"
+                + "'k': 10"
+                + "}, 'size': 10, 'ctl': { 'timeout': 10 } }) "
+                + f"LIMIT {limit};"
+            )
 
+        # Execute query after filtering by catalog_identifier if provided
         res, err = execute_query(cluster, filter_records_query)
         if err is not None:
             click.secho(f"ERROR: {err}", fg="red")
             return []
 
-        # catalog = CatalogDescriptor()
+        # List of catalog items from query
+        catalog = []
         for row in res.rows():
-            print(row)
-            # catalog.model_validate_json(row)
-
-        # print(catalog[0])
+            kind = row["record_kind"]
+            descriptor = ""
+            match kind:
+                case "semantic_search":
+                    descriptor = SemanticSearchToolDescriptor.model_validate(row)
+                case "python_function":
+                    descriptor = PythonToolDescriptor.model_validate(row)
+                case "sqlpp_query":
+                    descriptor = SQLPPQueryToolDescriptor.model_validate(row)
+                case "http_request":
+                    descriptor = HTTPRequestToolDescriptor.model_validate(row)
+                case _:
+                    print("not a valid descriptor of ToolDescriptorUnion type")
+            catalog.append(RecordDescriptor.model_validate(descriptor))
 
         # TODO: If annotations have been specified, prune all tools that do not possess these annotations.
 
-        # TODO: Perform semantic search
+        # Final set of results
+        results = [SearchResult(entry=cat, delta=0) for cat in catalog]
 
-        # TODO: Order results
-
-        # TODO: Apply our limit clause.
-        # if limit > 0:
-        #     results = results[:limit]
-        # return results
-
-        return []
+        return results
