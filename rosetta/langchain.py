2ae125d6824a967c32eca499ff86884524700278
Glenn Galvizo <glenn.galvizo@couchbase.com>
2024-08-30 10:14:25 -0700

    Rosetta now has proper support for serving / indexing prompts.
    Don't really see a use for multiple prompt serving, changing API here.
    
    Adding example of a prompt (where to include metadata, etc...)

--- rosetta/langchain.py
+++ rosetta/langchain.py
@@ -2,11 +2,20 @@ import importlib.util
 
 # We will only expose rosetta_lc if it exists.
 if importlib.util.find_spec("rosetta_lc") is not None:
+    import langchain_core.language_models.chat_models
+    import rosetta.auditor
+    import typing
+
     mod = importlib.import_module("rosetta_lc")
 
+    class AuditType(typing.Protocol):
+        def __call__(
+            self, chat_model: langchain_core.language_models.chat_models.BaseChatModel, auditor: rosetta.auditor = None
+        ) -> langchain_core.language_models.chat_models.BaseChatModel: ...
+
     # TODO (GLENN): Is there a less messy way to do this that doesn't erase symbols? (to keep IDEs happy)
-    audit = mod.audit
-    IQBackedChatModel = mod.IQBackedChatModel
+    audit: AuditType = mod.audit
+    IQBackedChatModel: langchain_core.language_models.chat_models.BaseChatModel = mod.IQBackedChatModel
     __all__ = ["audit", "IQBackedChatModel"]
 
 else:
