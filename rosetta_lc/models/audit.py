f198ff374f55263aaf19be0e18b1f5126b2bc787
Glenn Galvizo <glenn.galvizo@couchbase.com>
2024-09-09 13:02:02 -0700

    Removing dependency on rosetta

--- rosetta_lc/models/audit.py
+++ rosetta_lc/models/audit.py
@@ -1,5 +1,5 @@
 import logging
-import rosetta.auditor
+import rosetta_core
 import typing
 
 from langchain_core.callbacks import CallbackManagerForLLMRun
@@ -21,32 +21,28 @@ from langchain_core.outputs import ChatResult
 logger = logging.getLogger(__name__)
 
 
-def _determine_role_from_type(message: BaseMessage) -> rosetta.auditor.Role:
+def _determine_role_from_type(message: BaseMessage) -> rosetta_core.llm.Role:
     match type(message).__name__:
         case HumanMessage.__name__ | HumanMessageChunk.__name__:
-            return rosetta.auditor.Role.Human
+            return rosetta_core.llm.Role.Human
         case AIMessage.__name__ | AIMessageChunk.__name__:
-            return rosetta.auditor.Role.Assistant
+            return rosetta_core.llm.Role.Assistant
         case SystemMessage.__name__ | SystemMessageChunk.__name__:
-            return rosetta.auditor.Role.System
+            return rosetta_core.llm.Role.System
         case (
             ToolMessage.__name__
             | ToolMessageChunk.__name__
             | FunctionMessage.__name__
             | FunctionMessageChunk.__name__
         ):
-            return rosetta.auditor.Role.Tool
+            return rosetta_core.llm.Role.Tool
         case _:
             logger.debug(f'Unknown message type encountered: {message.type}. Tagging as "system".')
-            return rosetta.auditor.Role.System
+            return rosetta_core.llm.Role.System
 
 
-def audit(chat_model: BaseChatModel, session: typing.AnyStr, auditor: rosetta.Auditor = None) -> BaseChatModel:
+def audit(chat_model: BaseChatModel, session: typing.AnyStr, auditor: rosetta_core.auditor.Auditor) -> BaseChatModel:
     """A method to (dynamically) dispatch the '_generate' & '_stream' methods to methods that log LLM calls."""
-    if auditor is None:
-        # TODO (GLENN): Pull the LLM name from somewhere else (e.g., we aren't capturing gpt-4o).
-        auditor = rosetta.auditor.Auditor(llm_name=chat_model._llm_type)
-
     # TODO (GLENN): We should capture the _agenerate and _astream methods as well.
     generate_dispatch = chat_model._generate
     stream_dispatch = chat_model._stream
@@ -63,7 +59,7 @@ def audit(chat_model: BaseChatModel, session: typing.AnyStr, auditor: rosetta.Au
         results = generate_dispatch(messages, stop, run_manager, **kwargs)
         for result in results.generations:
             logger.debug(f"LLM has returned the message: {result}")
-            auditor.accept(rosetta.auditor.Role.Assistant, result.message.content, session)
+            auditor.accept(rosetta_core.llm.Role.Assistant, result.message.content, session)
         return results
 
     def _stream(
@@ -85,7 +81,7 @@ def audit(chat_model: BaseChatModel, session: typing.AnyStr, auditor: rosetta.Au
             yield chunk
 
         # We have exhausted our iterator. Log the resultant chunk.
-        auditor.accept(rosetta.auditor.Role.Assistant, result_text, session)
+        auditor.accept(rosetta_core.llm.Role.Assistant, result_text, session)
 
     # Note: Pydantic fiddles around with __setattr__, so we need to skirt around this.
     object.__setattr__(chat_model, "_generate", _generate.__get__(chat_model, BaseChatModel))
