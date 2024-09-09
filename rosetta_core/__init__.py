dd64a06ff4cd2abb8be6cc743aa756f53ae7c55a
Glenn Galvizo <glenn.galvizo@couchbase.com>
2024-09-09 13:06:38 -0700

    Removing llm from __all__

--- rosetta_core/__init__.py
+++ rosetta_core/__init__.py
@@ -1,10 +1,9 @@
 import rosetta_core.activity.auditor as auditor
 import rosetta_core.catalog as catalog
-import rosetta_core.llm as llm
 import rosetta_core.provider as provider
 import rosetta_core.tool as tool
 
-__all__ = ["tool", "provider", "catalog", "auditor", "llm"]
+__all__ = ["tool", "provider", "catalog", "auditor"]
 
 # This is the version of the SDK tool / library. For the version of the catalog data format, see
 # rosetta_core.catalog.VERSION_CATALOG.
