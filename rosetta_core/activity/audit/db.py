8a99332cf33ce09ca0ef55485981fa3fee1836e4
Tanvi Johari <tanvi.johari@couchbase.com>
2024-09-11 09:36:23 +0530

    DB auditor wip

--- rosetta_core/activity/audit/db.py
+++ rosetta_core/activity/audit/db.py
@@ -1,18 +1,66 @@
 import logging
 
+from ...defaults import DEFAULT_AUDIT_COLLECTION
+from ...defaults import DEFAULT_AUDIT_SCOPE
 from ...llm import Message
+from ...version import VersionDescriptor
 from .base import BaseAuditor
+from rosetta_cmd.models import CouchbaseConnect
+from rosetta_util.publish import create_scope_and_collection
+from rosetta_util.publish import get_connection
 
 logger = logging.getLogger(__name__)
 
 
 # TODO (GLENN): Implement this.
 class DBAuditor(BaseAuditor):
+    def __init__(self, bucket: str, secrets: dict, catalog_version: VersionDescriptor, model: str):
+        super().__init__(catalog_version, model)
+        self.secrets = secrets
+        conn = CouchbaseConnect(
+            connection_url=self.secrets["CB_CONN_STRING"],
+            username=self.secrets["CB_USERNAME"],
+            password=self.secrets["CB_PASSWORD"],
+        )
+        err, cluster = get_connection(conn)
+        if err is not None:
+            logger.error(err)
+            return
+
+        # Get bucket ref
+        cb = cluster.bucket(bucket)
+
+        # Get the bucket manager
+        bucket_manager = cb.collections()
+
+        msg, err = create_scope_and_collection(bucket_manager, DEFAULT_AUDIT_SCOPE, DEFAULT_AUDIT_COLLECTION)
+        if err is not None:
+            logger.error(err)
+            return
+
+        # get collection ref
+        cb_coll = cb.scope(DEFAULT_AUDIT_SCOPE).collection(DEFAULT_AUDIT_COLLECTION)
+
+        self.cb_coll = cb_coll
+        self.cluster = cluster
+
     def _accept(self, message: Message):
-        pass
+        cb_coll = self.cb_coll
+
+        # serialise message object to str
+        message_json = message.model_dump_json()
+        try:
+            key = message.timestamp
+            # upsert docs to CB collection
+            cb_coll.upsert(key, message_json)
+        except Exception as e:
+            logger.error("could not insert log: ", e)
+
+        return
 
     def close(self):
-        pass
+        self.cluster.close()
+        return
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
--- rosetta_core/defaults.py
+++ rosetta_core/defaults.py
@@ -1,5 +1,9 @@
 DEFAULT_SCOPE_PREFIX = "rosetta-catalog-"
+DEFAULT_AUDIT_SCOPE = "rosetta-logs"
+DEFAULT_AUDIT_COLLECTION = "rosetta-session-logs"
 
 __all__ = [
     "DEFAULT_SCOPE_PREFIX",
+    "DEFAULT_AUDIT_SCOPE",
+    "DEFAULT_AUDIT_COLLECTION",
 ]
