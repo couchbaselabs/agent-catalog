b1e8a2e9a93a3f8efaf600362f5b8d62ffdd2f4e
ThejasNU <thejas.nu@couchbase.com>
2024-10-14 11:17:05 +0530

    AV-87490_remove_controlflow

--- libs/agent_catalog_libs/cmd/cmds/execute.py
+++ libs/agent_catalog_libs/cmd/cmds/execute.py
@@ -2,72 +2,107 @@ import click
 import os
 import pathlib
 
-from libs.agent_catalog_libs.cmd.defaults import DEFAULT_TOOL_CATALOG_NAME
-from libs.agent_catalog_libs.cmd.models import Context
+from ..defaults import DEFAULT_TOOL_CATALOG_NAME
+from ..models import Context
 from libs.agent_catalog_libs.core.catalog import CatalogMem
+from libs.agent_catalog_libs.core.provider import ToolProvider
+from pydantic import PydanticSchemaGenerationError
+from pydantic import TypeAdapter
 
-supported_providers = ["openai", "azure-openai", "anthropic", "google", "groq"]
+types_mapping = {"array": list, "integer": int, "number": float, "string": str}
 
 
-def cmd_execute(ctx: Context, name: str, model: str):
-    import controlflow as cf
-
+def cmd_execute(ctx: Context, name: str):
     catalog_path = pathlib.Path(ctx.catalog) / DEFAULT_TOOL_CATALOG_NAME
     catalog = CatalogMem.load(catalog_path)
-    tool = catalog.find(name=name)
 
-    if len(tool) == 0:
-        raise ValueError(f"No tool {name} in the catalog!")
-
-    verify_provider_model_credentials(model)
-
-    tool_info = tool[0].entry
-    cf.settings.pretty_print_agent_events = False
-    cf.settings.prefect_log_level = "CRITICAL"
-    cf.settings.log_level = "CRITICAL"
-    agent = cf.Agent(
-        name="Test Agent",
-        model=model,
-        instructions=(
-            "You are a helper who helps in testing the tool provided by user, follows objectives given carefully."
-        ),
-    )
-
-    task1 = cf.Task(
-        objective="Print all the inputs required by user to give for the tool and mention the user to provide inputs separated by commas",
-        instructions="Take input from user as specified by the user and don't change anything in that or append anything to the user's input, keep it as it is. Don't prompt user multiple times, just take inputs and end the task.",
-        context=dict(tool=tool_info),
-        agents=[agent],
-        interactive=True,
-    )
-    inputs = task1.run()
-
-    task2 = cf.Task(
-        objective="User has given the inputs for the tool separated by commas, execute the tool and return the results as returned by the tool executed properly.",
-        context=dict(tool=tool_info, inputs=inputs),
-        agents=[agent],
-    )
-    results = task2.run()
-
-    click.secho("\nResults:", fg="green")
-    click.echo(results)
-
-
-def verify_provider_model_credentials(model: str):
-    model_info = model.split("/")
-
-    if len(model_info) != 2:
+    provider = ToolProvider(catalog, output=pathlib.Path(os.path.join(os.getcwd(), "codes")))
+    tool = provider.get(name)
+    try:
+        parameters = TypeAdapter(tool).json_schema()
+        class_types = dict()
+        if "$defs" in parameters:
+            class_types = get_types_for_classes(parameters["$defs"])
+        print(class_types)
+        input_types = dict()
+        for param, param_def in parameters["properties"].items():
+            if "$ref" in param_def:
+                input_types[param] = class_types[param_def["$ref"].split("/")[-1]]
+            elif param_def["type"] == "array":
+                input_types[param] = list[types_mapping[param_def["items"]["type"]]]
+            else:
+                input_types[param] = types_mapping[param_def["type"]]
+    except PydanticSchemaGenerationError:
         raise ValueError(
-            f"Invalid model {model} provided!\nProvide model name as {{provider}}/{{model_name}}.\nExample: openai/gpt-4o-mini"
-        )
+            f'Could not generate a schema for tool "{name}". '
+            "Tool functions must have type hints that are compatible with Pydantic."
+        ) from None
 
-    provider = model_info[0]
-    if provider not in ["openai", "azure-openai", "anthropic", "google", "groq"]:
-        raise ValueError(
-            f'Provided provider name {provider} is not supported!\nSupported providers are {", ".join(supported_providers)}.'
-        )
+    user_inputs = take_input_from_user(input_types)
 
-    if os.getenv(f"{provider.upper()}_API_KEY") is None:
-        raise ValueError(
-            f"No API key provided for the chosen provider!\nPlease set the api key as {provider.upper()}_API_KEY"
-        )
+    res = tool(**user_inputs)
+    click.secho("Result:", fg="green")
+    click.echo(res)
+
+
+def get_types_for_classes(class_defs: dict) -> dict:
+    class_types = dict()
+    for class_name, class_def in class_defs.items():
+        class_types[class_name] = dict()
+        for member_name, member_def in class_def["properties"].items():
+            if member_def["type"] == "array":
+                class_types[class_name][member_name] = list[types_mapping[member_def["items"]["type"]]]
+            else:
+                class_types[class_name][member_name] = types_mapping[member_def["type"]]
+    return class_types
+
+
+def take_input_from_user(input_types: dict) -> dict:
+    user_inputs = dict()
+    for inp, inp_type in input_types.items():
+        if isinstance(inp_type, dict):
+            user_inputs[inp] = dict()
+            for inp_member, inp_member_type in inp_type.items():
+                is_list = inp_member_type in [list[str], list[int], list[float]]
+                entered_val = click.prompt(f"{inp_member}", type=str if is_list else inp_member_type)
+                if not is_list:
+                    user_inputs[inp][inp_member] = entered_val
+                    continue
+
+                list_type = "string"
+                if inp_member_type == list[int]:
+                    list_type = "integer"
+                elif inp_member_type == list[float]:
+                    list_type = "number"
+
+                is_correct = True
+                try:
+                    conv_inps = split_and_convert(entered_val, types_mapping[list_type])
+                    user_inputs[inp][inp_member] = conv_inps
+                    continue
+                except ValueError:
+                    is_correct = False
+
+                while not is_correct:
+                    click.echo(f"All entered values are not of type {list_type}! Enter correct values")
+                    entered_val = click.prompt(f"{inp_member}", type=str if is_list else inp_member_type)
+                    try:
+                        conv_inps = split_and_convert(entered_val, types_mapping[list_type])
+                        is_correct = True
+                        user_inputs[inp][inp_member] = conv_inps
+                        break
+                    except ValueError:
+                        is_correct = False
+
+        else:
+            entered_val = click.prompt(f"{inp}", type=inp_type)
+            user_inputs[inp] = entered_val
+    return user_inputs
+
+
+def split_and_convert(entered_val: str, target_type):
+    conv_inps = []
+    for element in entered_val.split(","):
+        element = element.strip()
+        conv_inps.append(target_type(element))
+    return conv_inps
