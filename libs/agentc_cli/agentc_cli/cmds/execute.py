08b5daadfdc7929389e94fd4a3903abac319049a
ThejasNU <thejas.nu@couchbase.com>
2024-10-18 15:47:51 +0530

    AV-87490_add_support_for_classes

--- libs/agentc_cli/agentc_cli/cmds/execute.py
+++ libs/agentc_cli/agentc_cli/cmds/execute.py
@@ -1,6 +1,7 @@
 import click
-import os
+import importlib
 import pathlib
+import tempfile
 
 from ..models import Context
 from agentc_core.catalog import CatalogMem
@@ -12,50 +13,80 @@ from pydantic import TypeAdapter
 types_mapping = {"array": list, "integer": int, "number": float, "string": str}
 
 
-def cmd_execute(ctx: Context, name: str | None, query: str | None):
+def cmd_execute(ctx: Context, name: str | None, query: str | None, embedding_model: str):
+    # get local catalog
     catalog_path = pathlib.Path(ctx.catalog) / DEFAULT_TOOL_CATALOG_NAME
-    catalog = CatalogMem.load(catalog_path)
+    catalog = CatalogMem.load(catalog_path=catalog_path, embedding_model=embedding_model)
 
-    provider = ToolProvider(catalog, output=pathlib.Path(os.path.join(os.getcwd(), "codes")))
-    tool = None
-    if name is not None:
-        tool = provider.get(name)
-        if tool is None:
-            raise ValueError(f"Tool {name} not found!") from None
-    else:
-        tools = provider.search(query, limit=1)
-        if len(tools) == 0:
-            raise ValueError(f"No tool available for query {query}!") from None
+    # create temp directory for code dump
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        provider = ToolProvider(catalog, output=pathlib.Path(tmp_dir), decorator=lambda x: x)
+
+        # based on name or query get appropriate tool
+        if name is not None:
+            tool = provider.get(name)
+            if tool is None:
+                raise ValueError(f"Tool {name} not found!") from None
         else:
-            tool = tools[0]
-
-    try:
-        parameters = TypeAdapter(tool).json_schema()
-        class_types = dict()
-        if "$defs" in parameters:
-            class_types = get_types_for_classes(parameters["$defs"])
-        print(class_types)
-        input_types = dict()
-        for param, param_def in parameters["properties"].items():
-            if "$ref" in param_def:
-                input_types[param] = class_types[param_def["$ref"].split("/")[-1]]
-            elif param_def["type"] == "array":
-                input_types[param] = list(types_mapping[param_def["items"]["type"]])
+            tools = provider.search(query, limit=1)
+            if len(tools) == 0:
+                raise ValueError(f"No tool available for query {query}!") from None
             else:
-                input_types[param] = types_mapping[param_def["type"]]
-    except PydanticSchemaGenerationError:
-        raise ValueError(
-            f'Could not generate a schema for tool "{name}". '
-            "Tool functions must have type hints that are compatible with Pydantic."
-        ) from None
+                tool = tools[0]
 
-    user_inputs = take_input_from_user(input_types)
+        # get tool metadata
+        tool_metadata = tool.meta
 
-    res = tool(**user_inputs)
-    click.secho("Result:", fg="green")
-    click.echo(res)
+        # extract all variables that user needs to provide as input for tool
+        try:
+            parameters = TypeAdapter(tool.func).json_schema()
+            class_types = dict()
+            # get types for all custom defined classes
+            if "$defs" in parameters:
+                class_types = get_types_for_classes(parameters["$defs"])
+            input_types = dict()
+            for param, param_def in parameters["properties"].items():
+                # class type
+                if "$ref" in param_def:
+                    input_types[param] = class_types[param_def["$ref"].split("/")[-1]]
+                # list type
+                elif param_def["type"] == "array":
+                    input_types[param] = list(types_mapping[param_def["items"]["type"]])
+                # other types like str, int, float
+                else:
+                    input_types[param] = types_mapping[param_def["type"]]
+        except PydanticSchemaGenerationError:
+            raise ValueError(
+                f'Could not generate a schema for tool "{name}". '
+                "Tool functions must have type hints that are compatible with Pydantic."
+            ) from None
+
+        # import directly from python file if it is python tool
+        if ".py" in str(tool_metadata.source):
+            gen_code_modules = importlib.import_module(str(tool_metadata.source).split(".py")[0])
+
+        # TODO: import similarly to python tool for others from the temp directory
+
+        # prompt user for inputs
+        user_inputs = take_input_from_user(input_types)
+
+        # if user has any variable which is of object type, create it from class
+        modified_user_inputs = dict()
+        for variable, user_input in user_inputs.items():
+            if isinstance(user_input, dict) and "$ref" in parameters["properties"][variable]:
+                custom_class_name = parameters["properties"][variable]["$ref"].split("/")[-1]
+                class_needed = getattr(gen_code_modules, custom_class_name)
+                modified_user_inputs[variable] = class_needed(**user_input)
+            else:
+                modified_user_inputs[variable] = user_input
+
+        # call tool function
+        res = tool.func(**modified_user_inputs)
+        click.secho("Result:", fg="green")
+        click.echo(res)
 
 
+# gets all class variable types present in all custom defined classes in code
 def get_types_for_classes(class_defs: dict) -> dict:
     class_types = dict()
     for class_name, class_def in class_defs.items():
@@ -68,14 +99,23 @@ def get_types_for_classes(class_defs: dict) -> dict:
     return class_types
 
 
+# takes input from user based on the types provided
 def take_input_from_user(input_types: dict) -> dict:
     user_inputs = dict()
     for inp, inp_type in input_types.items():
         if isinstance(inp_type, dict):
             user_inputs[inp] = dict()
             for inp_member, inp_member_type in inp_type.items():
+                # if list ask user to provide inputs separated by commas and process later
                 is_list = inp_member_type in [list[str], list[int], list[float]]
-                entered_val = click.prompt(f"{inp_member}", type=str if is_list else inp_member_type)
+                inp_type_to_show_user = (
+                    f"{inp_member_type.__origin__.__name__} [{', '.join(arg.__name__ for arg in inp_member_type.__args__)}]"
+                    if is_list
+                    else inp_member_type.__name__
+                )
+                entered_val = click.prompt(
+                    f"{inp_member} ({inp_type_to_show_user})", type=str if is_list else inp_member_type
+                )
                 if not is_list:
                     user_inputs[inp][inp_member] = entered_val
                     continue
@@ -86,6 +126,8 @@ def take_input_from_user(input_types: dict) -> dict:
                 elif inp_member_type == list[float]:
                     list_type = "number"
 
+                # check if all comma separated values are of desired type
+                # else keep asking in the loop till correct values are given
                 is_correct = True
                 try:
                     conv_inps = split_and_convert(entered_val, types_mapping[list_type])
@@ -96,7 +138,9 @@ def take_input_from_user(input_types: dict) -> dict:
 
                 while not is_correct:
                     click.echo(f"All entered values are not of type {list_type}! Enter correct values")
-                    entered_val = click.prompt(f"{inp_member}", type=str if is_list else inp_member_type)
+                    entered_val = click.prompt(
+                        f"{inp_member} ({inp_type_to_show_user})", type=str if is_list else inp_member_type
+                    )
                     try:
                         conv_inps = split_and_convert(entered_val, types_mapping[list_type])
                         is_correct = True
@@ -104,13 +148,13 @@ def take_input_from_user(input_types: dict) -> dict:
                         break
                     except ValueError:
                         is_correct = False
-
         else:
-            entered_val = click.prompt(f"{inp}", type=inp_type)
+            entered_val = click.prompt(f"{inp} ({inp_type.__name__})", type=inp_type)
             user_inputs[inp] = entered_val
     return user_inputs
 
 
+# extract each value from comma separated values and convert to desired type
 def split_and_convert(entered_val: str, target_type):
     conv_inps = []
     for element in entered_val.split(","):
