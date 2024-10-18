850246f6e2c900ea25bc132b1c3ceb1177b4aa16
Tanvi Johari <tanvi.johari@couchbase.com>
2024-10-18 19:00:28 +0530

    Modified github actions

--- libs/agentc_cli/tests/couchbase_server_start.sh
+++ libs/agentc_cli/tests/couchbase_server_start.sh
@@ -5,11 +5,19 @@
 docker rm -f smoke_tests_db
 
 # Start server from latest couchbase docker image
+echo "Starting Couchbase container..."
 docker run -d --name smoke_tests_db -p 8091-8096:8091-8096 -p 11210-11211:11210-11211 couchbase
-sleep 5
+if [ $? -ne 0 ]; then
+  echo "Failed to start Couchbase container!"
+  exit 1
+fi
+
+echo "Waiting for Couchbase to initialize..."
+sleep 10
 
 # Initialise cluster
-curl -X POST http://localhost:8091/clusterInit \
+echo "Initializing Couchbase cluster..."
+curl -v -X POST http://localhost:8091/clusterInit \
   -d "username=Administrator" \
   -d "password=password" \
   -d "sendStats=true" \
@@ -17,11 +25,22 @@ curl -X POST http://localhost:8091/clusterInit \
   -d "services=kv,n1ql,index,fts" \
   -d port='SAME' \
   -d indexerStorageMode='plasma'
-sleep 5
+if [ $? -ne 0 ]; then
+  echo "Failed to initialize cluster!"
+  exit 1
+fi
 
 # Import travel-sample bucket
-curl -X POST -u Administrator:password \
+echo "Importing travel-sample bucket..."
+curl -v -X POST -u Administrator:password \
    http://localhost:8091/sampleBuckets/install \
    -d '["travel-sample"]' | jq .
+
+if [ $? -ne 0 ]; then
+  echo "Failed to import travel-sample bucket!"
+  exit 1
+fi
+
+echo "Couchbase setup complete. Cluster and travel-sample bucket successfully initialized!"
 
 #docker stop smoke_tests_db
