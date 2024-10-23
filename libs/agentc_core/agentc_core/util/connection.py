6a08ba2b38885239df3c000be95073504855a416
ThejasNU <thejas.nu@couchbase.com>
2024-10-23 12:11:01 +0530

    AV-87490_update_to_new_catalog
    handle no url in .env

--- libs/agentc_core/agentc_core/util/connection.py
+++ libs/agentc_core/agentc_core/util/connection.py
@@ -1,4 +1,7 @@
 def get_host_name(url: str):
+    if url is None:
+        raise ValueError("Could not find Couchbase connection URL in the environment variables file!")
+
     split_url = url.split("//")
     num_elements = len(split_url)
     if num_elements == 2:
