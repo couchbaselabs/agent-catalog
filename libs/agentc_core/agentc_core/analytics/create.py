bbb77fadf8c0414203dfe3bc966ac8f6cc78be0b
Glenn Galvizo <glenn.galvizo@couchbase.com>
2024-11-01 11:20:11 -0700

    creating analytics scope and collections if they do not exist.

--- libs/agentc_core/agentc_core/analytics/create.py
+++ libs/agentc_core/agentc_core/analytics/create.py
@@ -29,6 +29,25 @@ logger = logging.getLogger(__name__)
 
 
 def create_analytics_udfs(cluster: couchbase.cluster.Cluster, bucket: str) -> None:
+    logger.debug("Creating analytics log scope.")
+    ddl_result = cluster.analytics_query(f"""
+        CREATE ANALYTICS SCOPE `{bucket}`.`{DEFAULT_AUDIT_SCOPE}`
+        IF NOT EXISTS;
+    """)
+    for _ in ddl_result.rows():
+        pass
+
+    logger.debug("Creating analytics log collection.")
+    ddl_result = cluster.analytics_query(f"""
+        CREATE ANALYTICS COLLECTION
+        IF NOT EXISTS
+        `{bucket}`.`{DEFAULT_AUDIT_SCOPE}`.`{DEFAULT_AUDIT_COLLECTION}`
+        ON `{bucket}`.`{DEFAULT_AUDIT_SCOPE}`.`{DEFAULT_AUDIT_COLLECTION}`;
+    """)
+    for _ in ddl_result.rows():
+        pass
+
+    # Onto to our UDF DDLs...
     ddls_folder = pathlib.Path(__file__).parent / "ddls"
     ddl_files = sorted(file for file in ddls_folder.iterdir())
     for ddl_file in ddl_files:
@@ -42,7 +61,6 @@ def create_analytics_udfs(cluster: couchbase.cluster.Cluster, bucket: str) -> No
             )
             logger.debug(f"Issuing the following statement: {ddl_string}")
 
-            # TODO (GLENN): There should be a warning here (instead of an error) if Analytics is not enabled.
             ddl_result = cluster.analytics_query(ddl_string)
             for _ in ddl_result.rows():
                 pass
