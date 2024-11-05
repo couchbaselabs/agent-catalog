33f359cbbf16f471d2f95f83990949f9ea642aba
Tanvi Johari <tanvi.johari@couchbase.com>
2024-11-05 11:35:42 +0530

    Modifying github actions for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ extensions = [
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.viewcode",
     "sphinx.ext.todo",
-    "sphinx.ext.autodoc_pydantic",
+    "sphinxcontrib.autodoc_pydantic",
     "sphinx_copybutton",
     "sphinx_click",
     "sphinx.ext.githubpages",
