284425aaad1f0ce7102633f6d15a7331d9e8868e
ThejasNU <thejas.nu@couchbase.com>
2024-11-18 16:02:55 +0530

    refactor sql tool

--- prebuilt/tools/nl2sql_tools.py
+++ prebuilt/tools/nl2sql_tools.py
@@ -1,78 +1,68 @@
 import agentc
-import os
+import json
+import logging
+import requests
+
+from agentc_core.secrets import get_secret
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster
+from couchbase.options import ClusterOptions
+from datetime import timedelta
+
+logger = logging.getLogger()
+
+
+def _get_couchbase_cluster() -> Cluster:
+    authenticator = PasswordAuthenticator(
+        username=get_secret("CB_USERNAME").get_secret_value(), password=get_secret("CB_PASSWORD").get_secret_value()
+    )
+    conn_string = get_secret("CB_CONN_STRING").get_secret_value()
+    options = ClusterOptions(authenticator)
+    options.apply_profile("wan_development")
+    cluster = Cluster(conn_string, options)
+    cluster.wait_until_ready(timedelta(seconds=15))
+    return cluster
+
+
+def _extract_schema(bucket: str, scope: str, collection: str, cluster: Cluster) -> dict:
+    """Extracts schema of collection from couchbase cluster"""
+    try:
+        result = cluster.query(f"INFER `{bucket}`.`{scope}`.`{collection}`;").execute()
+        inferred_schema = result[0]
+        properties = inferred_schema[0]["properties"]
+        field_types = {}
+        for field, info in properties.items():
+            field_types[field] = info["type"]
+        return field_types
+    except Exception as ex:
+        logger.error("Error extracting schema :", ex)
+        return {}
 
 
 @agentc.tool
-def iq_tool(
-    bucket, collection, scope, username, password, cluster_url, jwt_token, capella_address, org_id, natural_query
-):
-    """generate SQL++ query for given natural language query"""
-    import json
-    import logging
-    import requests
-    import traceback
+def iq_tool(bucket: str, scope: str, collection: str, natural_lang_query: str) -> str:
+    """Takes in natural language query that has to be performed on Couchbase cluster and returns SQL++ query for it, which can be executed later on the cluster."""
 
-    from couchbase.auth import PasswordAuthenticator
-    from couchbase.cluster import Cluster
-    from couchbase.exceptions import CouchbaseException
-    from couchbase.options import ClusterOptions
-    from datetime import timedelta
-
-    logger = logging.getLogger()
-
-    def extract_schema(bucket, collection, scope, username, password, cluster_url) -> dict:
-        """
-            Extracts schema of collection from couchbase cluster
-
-            Args:
-            naturl_query, bucket, scope, collection, username, password, cluster_url
-
-            Returns:
-        (dict) Schema
-        """
-
-        try:
-            # cluster connection
-            if cluster_url == "couchbase://127.0.0.1" or cluster_url == "couchbase://localhost":
-                auth = PasswordAuthenticator(username, password)
-                options = ClusterOptions(auth)
-                options.apply_profile("wan_development")
-            else:
-                auth = PasswordAuthenticator(username, password, cert_path="certificates/cert.pem")
-                options = ClusterOptions(auth)
-                options.apply_profile("wan_development")
-
-            try:
-                cluster = Cluster(cluster_url, options)
-                cluster.wait_until_ready(timedelta(seconds=15))
-            except CouchbaseException as e:
-                return {"status": False, "message": f"Error connecting to couchbase : {e}"}
-
-            result = cluster.query(f"INFER `{bucket}`.`{scope}`.`{collection}`;")
-            for row in result:
-                inferred_schema = row
-            properties = inferred_schema[0]["properties"]
-            field_types = {}
-            for field, data in properties.items():
-                field_types[field] = data["type"]
-            return field_types
-        except Exception as ex:
-            logger.error("Error extracting schema :", ex)
-            return {}
-
-    url = f"{capella_address}/v2/organizations/{org_id}/integrations/iq/openai/chat/completions"
-    # url = f"https://api.openai.com/v1/chat/completions"
-
-    headers = {"Authorization": f"Bearer {jwt_token}", "Content-Type": "application/json"}
+    # Get all required secrets
+    capella_address = get_secret("CAPELLA_CP_ADDRESS").get_secret_value()
+    org_id = get_secret("CAPELLA_ORG_ID").get_secret_value()
+    jwt_token = get_secret("CAPELLA_JWT_TOKEN").get_secret_value()
 
     # Extract schema
-    schema = extract_schema(bucket, collection, scope, username, password, cluster_url)
+    cluster = _get_couchbase_cluster()
+    if cluster is None:
+        return ""
 
+    schema = _extract_schema(bucket, scope, collection, cluster)
+
+    # Make call to iQ proxy
+    url = f"{capella_address}/v2/organizations/{org_id}/integrations/iq/openai/chat/completions"
+    headers = {"Authorization": f"Bearer {jwt_token}", "Content-Type": "application/json"}
     payload = {
         "messages": [
             {
                 "role": "user",
-                "content": f'Generate ONLY a valid SQL++ query based on the following natural language prompt. Return the query JSON with field as query, without any natural language text and WITHOUT MARKDOWN syntax in the query.\n\nNatural language prompt: \n"""\n{natural_query}\n"""\n .If the natural language prompt can be used to generate a query:\n- query using follwing bucket - {bucket}, scope - {scope} and collection - {collection}. Heres the schema {schema}.\n. For queries involving SELECT statements use ALIASES LIKE the following EXAMPLE: `SELECT a.* FROM <collection> as a LIMIT 10;` instead of `SELECT * FROM <collection> LIMIT 10;` STRICTLY USE A.* OR SOMETHING SIMILAR \nIf the natural language prompt cannot be used to generate a query, write an error message and return as JSON with field as error.',
+                "content": f'Generate ONLY a valid SQL++ query based on the following natural language prompt. Return the query JSON with field as query, without any natural language text and WITHOUT MARKDOWN syntax in the query.\n\nNatural language prompt: \n"""\n{natural_lang_query}\n"""\n .If the natural language prompt can be used to generate a query:\n- query using follwing bucket - {bucket}, scope - {scope} and collection - {collection}. Heres the schema {schema}.\n. For queries involving SELECT statements use ALIASES LIKE the following EXAMPLE: `SELECT a.* FROM <collection> as a LIMIT 10;` instead of `SELECT * FROM <collection> LIMIT 10;` STRICTLY USE A.* OR SOMETHING SIMILAR \nIf the natural language prompt cannot be used to generate a query, write an error message and return as JSON with field as error.',
             }
         ],
         "initMessages": [
@@ -88,63 +78,13 @@ def iq_tool(
         "completionSettings": {"model": "gpt-3.5-turbo", "temperature": 0, "max_tokens": 1024, "stream": False},
     }
 
-    natural_language_query = None
+    sqlpp_query = ""
+    res = requests.post(url, headers=headers, json=payload)
+    res_json = res.json()
+    res_dict = json.loads(res_json["choices"][0]["message"]["content"])
 
-    try:
-        # API call to LLM
-        res = requests.post(url, headers=headers, json=payload)
-        res_json = res.json()
-        print(res_json)
-        res_dict = json.loads(res_json["choices"][0]["message"]["content"])
+    # Check if the query is generated
+    if "query" in res_dict:
+        sqlpp_query = res_dict["query"]
 
-        # Check if the query is generated
-        if "query" in res_dict:
-            natural_language_query = res_dict["query"]
-
-    except requests.exceptions.RequestException as e:
-        return {"status": False, "Message": f"Error generating SQL++ query : {str(e)}"}
-    except Exception:
-        traceback.print_exc()
-
-    cluster = None
-
-    if cluster_url == "couchbase://127.0.0.1" or cluster_url == "couchbase://localhost":
-        auth = PasswordAuthenticator(username, password)
-        options = ClusterOptions(auth)
-        options.apply_profile("wan_development")
-    else:
-        auth = PasswordAuthenticator(username, password, cert_path="certificates/cert.pem")
-        options = ClusterOptions(auth)
-        options.apply_profile("wan_development")
-
-    try:
-        cluster = Cluster(cluster_url, options)
-        cluster.wait_until_ready(timedelta(seconds=15))
-    except CouchbaseException as e:
-        return {"status": False, "message": f"Error connecting to couchbase : {e}"}
-
-    data = []
-    if natural_language_query:
-        result = cluster.query(natural_language_query)
-        for row in result:
-            data.append(row)
-        # each row is an instance of the query call
-    return data
-
-
-if __name__ == "__main__":
-    print(os.getenv("CB_JWT_TOKEN"), "token")
-
-    data = iq_tool(
-        "travel-sample",
-        "airport",
-        "inventory",
-        "Administrator",
-        "password",
-        "couchbase://127.0.0.1",
-        os.getenv("CB_JWT_TOKEN"),
-        "https://api.dev.nonprod-project-avengers.com",
-        "6af08c0a-8cab-4c1c-b257-b521575c16d0",
-        "find 5 flights in the united states",
-    )
-    print(data)
+    return sqlpp_query
