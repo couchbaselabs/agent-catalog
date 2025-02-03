ea690beb75b65bb51cf25d6ec3c3b47ff03c40ce
ThejasNU <thejas.nu@couchbase.com>
2025-02-03 11:31:21 +0530

    add annotation for test

--- libs/agentc_core/tests/catalog/test_scan_dir.py
+++ libs/agentc_core/tests/catalog/test_scan_dir.py
@@ -1,11 +1,13 @@
 import os
 import pathlib
+import pytest
 
 from agentc_core.catalog.directory import scan_directory
 from agentc_core.defaults import DEFAULT_SCAN_DIRECTORY_OPTS
 from agentc_core.indexer.indexer import AllIndexers
 
 
+@pytest.mark.smoke
 def test_scan_dir_tools():
     root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "scan_files")
     source_globs = [i.glob_pattern for i in AllIndexers if all(k.is_tool() for k in i.kind)]
@@ -20,6 +22,7 @@ def test_scan_dir_tools():
     )
 
 
+@pytest.mark.smoke
 def test_scan_dir_prompts():
     root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "scan_files")
     source_globs = [i.glob_pattern for i in AllIndexers if all(k.is_prompt() for k in i.kind)]
