a910fad6269b039789e67133298293f9254704e3
ThejasNU <thejas.nu@couchbase.com>
2025-02-03 11:12:02 +0530

    add tests

--- libs/agentc_core/agentc_core/catalog/directory.py
+++ libs/agentc_core/agentc_core/catalog/directory.py
@@ -14,7 +14,7 @@ class ScanDirectoryOpts(typing.TypedDict):
 
 
 def scan_directory(
-    root_dir: str, wanted_patterns: typing.Iterable[str], opts: ScanDirectoryOpts = None
+    root_dir: str, target_dir: str, wanted_patterns: typing.Iterable[str], opts: ScanDirectoryOpts = None
 ) -> typing.Iterable[pathlib.Path]:
     """
     Find file paths in a directory tree which match wanted glob patterns, while also handling any ignore
@@ -23,29 +23,28 @@ def scan_directory(
 
     ignore_file_parsers = []
     all_ignore_files_paths = []
-    if opts:
-        current_dir = os.getcwd()
-        user_target_dir = os.path.abspath(os.path.join(current_dir, root_dir))
+    user_target_dir = os.path.abspath(os.path.join(root_dir, target_dir))
 
+    if opts:
         # Find all ignore files in the directory tree till user mentioned directory.
-        for root, _dirs, files in os.walk(current_dir):
+        for cur_dir, _dirs, files in os.walk(root_dir):
             # Ignore path if it does not appear in the path towards user mentioned directory.
-            if root not in user_target_dir:
+            if cur_dir not in user_target_dir:
                 continue
 
             for file in files:
                 if file in opts["ignore_file_names"]:
-                    all_ignore_files_paths.append(os.path.join(root, file))
+                    all_ignore_files_paths.append(os.path.join(cur_dir, file))
 
             # Stop crawling once user mentioned directory is crawled.
-            if root == user_target_dir:
+            if cur_dir == user_target_dir:
                 break
 
         if opts["ignore_file_parser_factory"]:
             for ignore_file_path in all_ignore_files_paths:
                 ignore_file_parsers.append(opts["ignore_file_parser_factory"](ignore_file_path))
 
-    for path in pathlib.Path(root_dir).rglob("*"):
+    for path in pathlib.Path(user_target_dir).rglob("*"):
         if len(ignore_file_parsers) > 0 and any(ignore_file_parser(path) for ignore_file_parser in ignore_file_parsers):
             logger.debug(f"Ignoring file {path.absolute()}.")
             continue
@@ -60,5 +59,5 @@ if __name__ == "__main__":
     import sys
 
     # Ex: python3 agentc_core/catalog/directory.py "*.py" "*.md"
-    for x in scan_directory("", sys.argv[1:]):
+    for x in scan_directory("", "", sys.argv[1:]):
         print(x)
