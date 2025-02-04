77146a49ae781a4a931b8f8e6f97c16b96dadff0
ThejasNU <thejas.nu@couchbase.com>
2025-02-04 00:49:19 -0800

    add logs deletion based on time

--- libs/agentc_cli/agentc_cli/cmds/clean.py
+++ libs/agentc_cli/agentc_cli/cmds/clean.py
@@ -1,132 +1,176 @@
-import click_extra
+import click
 import couchbase.cluster
+import dateparser
 import importlib.util
 import logging
-import os
 import pathlib
-import shutil
 import typing
 
-from .util import logging_command
-from agentc_core.config import Config
-from agentc_core.defaults import DEFAULT_ACTIVITY_FOLDER
-from agentc_core.defaults import DEFAULT_ACTIVITY_SCOPE
-from agentc_core.defaults import DEFAULT_CATALOG_FOLDER
-from agentc_core.defaults import DEFAULT_CATALOG_METADATA_COLLECTION
-from agentc_core.defaults import DEFAULT_CATALOG_PROMPT_COLLECTION
+from ..models.context import Context
+from .util import remove_directory
+from agentc_core.analytics import Log
+from agentc_core.defaults import DEFAULT_AUDIT_COLLECTION
+from agentc_core.defaults import DEFAULT_AUDIT_SCOPE
+from agentc_core.defaults import DEFAULT_CATALOG_COLLECTION_NAME
 from agentc_core.defaults import DEFAULT_CATALOG_SCOPE
-from agentc_core.defaults import DEFAULT_CATALOG_TOOL_COLLECTION
-from agentc_core.remote.util.query import execute_query
+from agentc_core.defaults import DEFAULT_LLM_ACTIVITY_NAME
+from agentc_core.defaults import DEFAULT_META_COLLECTION_NAME
+from agentc_core.util.query import execute_query
+from pydantic import ValidationError
 from typing_extensions import Literal
+from tzlocal import get_localzone
 
 logger = logging.getLogger(__name__)
 
 
-# TODO (GLENN): We should add some granularity w.r.t. what to clean here?
-def clean_local(cfg: Config, targets: list[Literal["catalog", "activity"]]):
-    target_folders = []
-    if "catalog" in targets:
-        target_folders.append(DEFAULT_CATALOG_FOLDER)
-    if "activity" in targets:
-        target_folders.append(DEFAULT_ACTIVITY_FOLDER)
+def clean_local(ctx: Context | None, type_metadata: str, date: str = None):
+    clean_catalog = type_metadata == "catalog" or type_metadata == "all"
+    clean_activity = type_metadata == "activity" or type_metadata == "all"
 
-    for folder in target_folders:
-        if not folder or not os.path.exists(folder):
-            continue
-        folder_path = pathlib.Path(folder)
-        if folder_path.is_file():
-            os.remove(folder_path.absolute())
-        elif folder_path.is_dir():
-            shutil.rmtree(folder_path.absolute())
+    if clean_catalog:
+        remove_directory(ctx.catalog)
+
+    if clean_activity:
+        if date is not None:
+            req_date = dateparser.parse(date)
+            local_tz = get_localzone()
+            req_date = req_date.replace(tzinfo=local_tz)
+
+            if req_date is None:
+                raise ValueError(f"Invalid date provided: {date}")
+
+            log_path = pathlib.Path(ctx.activity) / DEFAULT_LLM_ACTIVITY_NAME
+            try:
+                with log_path.open("r+") as fp:
+                    # read all lines before editing the file
+                    lines = fp.readlines()
+                    # move file pointer to the beginning of a file
+                    fp.seek(0)
+                    for line in lines:
+                        try:
+                            cur_log = Log.model_validate_json(line.strip())
+                            cur_log_timestamp = dateparser.parse(cur_log.timestamp.isoformat())
+                            if cur_log_timestamp >= req_date:
+                                fp.write(line)
+                        except ValidationError as e:
+                            logger.error(f"Invalid log entry: {e}")
+                    # truncate the file
+                    fp.truncate()
+            except FileNotFoundError:
+                raise ValueError("No log file found! Please run auditor!") from None
+
+        else:
+            remove_directory(ctx.activity)
 
 
 def clean_db(
-    cfg: Config,
+    ctx: Context | None,
+    bucket: str,
+    cluster: couchbase.cluster.Cluster,
     catalog_ids: list[str],
-    kind: list[typing.Literal["tool", "prompt"]],
-    targets: list[Literal["catalog", "activity"]],
+    kind_list: list[typing.Literal["tool", "prompt"]],
+    type_metadata: Literal["catalog", "activity", "all"],
+    date: str = None,
 ) -> int:
-    cluster: couchbase.cluster.Cluster = cfg.Cluster()
+    all_errs = []
+    clean_catalog = type_metadata == "catalog" or type_metadata == "all"
+    clean_activity = type_metadata == "activity" or type_metadata == "all"
 
-    # TODO (GLENN): Is there a reason we are accumulating errors here (instead of stopping on the first error)?
-    all_errs = list()
-    if len(catalog_ids) > 0:
-        for k in kind:
-            click_extra.secho(f"Removing catalog(s): {[catalog for catalog in catalog_ids]}", fg="yellow")
-            meta_catalog_condition = " AND ".join([f"version.identifier = '{catalog}'" for catalog in catalog_ids])
-            remove_metadata_query = f"""
-                DELETE FROM
-                    `{cfg.bucket}`.`{DEFAULT_CATALOG_SCOPE}`.{DEFAULT_CATALOG_METADATA_COLLECTION}
-                WHERE
-                    kind = "{k}" AND
-                    {meta_catalog_condition};
-            """
-            res, err = execute_query(cluster, remove_metadata_query)
+    if clean_catalog:
+        if len(catalog_ids) > 0:
+            for kind in kind_list:
+                click.secho(f"Removing catalog(s): {[catalog for catalog in catalog_ids]}", fg="yellow")
+
+                catalog_condition = " AND ".join([f"catalog_identifier = '{catalog}'" for catalog in catalog_ids])
+                meta_catalog_condition = " AND ".join([f"version.identifier = '{catalog}'" for catalog in catalog_ids])
+                remove_catalogs_query = f"""
+                                DELETE FROM
+                                    `{bucket}`.`{DEFAULT_CATALOG_SCOPE}`.{kind}{DEFAULT_CATALOG_COLLECTION_NAME}
+                                WHERE
+                                    {catalog_condition};
+                            """
+                remove_metadata_query = f"""
+                                DELETE FROM
+                                    `{bucket}`.`{DEFAULT_CATALOG_SCOPE}`.{kind}{DEFAULT_META_COLLECTION_NAME}
+                                WHERE
+                                    {meta_catalog_condition};
+                            """
+
+                res, err = execute_query(cluster, remove_catalogs_query)
+                for r in res.rows():
+                    logger.debug(r)
+                if err is not None:
+                    all_errs.append(err)
+
+                res, err = execute_query(cluster, remove_metadata_query)
+                for r in res.rows():
+                    logger.debug(r)
+                if err is not None:
+                    all_errs.append(err)
+
+        else:
+            drop_scope_query = f"DROP SCOPE `{bucket}`.`{DEFAULT_CATALOG_SCOPE}` IF EXISTS;"
+            res, err = execute_query(cluster, drop_scope_query)
             for r in res.rows():
                 logger.debug(r)
             if err is not None:
                 all_errs.append(err)
 
-            collection = DEFAULT_CATALOG_TOOL_COLLECTION if k == "tool" else DEFAULT_CATALOG_PROMPT_COLLECTION
-            catalog_condition = " AND ".join([f"catalog_identifier = '{catalog}'" for catalog in catalog_ids])
+    if clean_activity:
+        if date is not None:
+            req_date = dateparser.parse(date)
+            local_tz = get_localzone()
+            req_date = req_date.replace(tzinfo=local_tz)
+
             remove_catalogs_query = f"""
-                DELETE FROM
-                    `{cfg.bucket}`.`{DEFAULT_CATALOG_SCOPE}`.`{collection}`
-                WHERE
-                    {catalog_condition};
-            """
+                            DELETE FROM
+                                `{bucket}`.`{DEFAULT_AUDIT_SCOPE}`.`{DEFAULT_AUDIT_COLLECTION}`
+                            WHERE
+                                STR_TO_MILLIS(timestamp) < STR_TO_MILLIS('{req_date.isoformat()}');
+                        """
+
             res, err = execute_query(cluster, remove_catalogs_query)
+
             for r in res.rows():
                 logger.debug(r)
             if err is not None:
                 all_errs.append(err)
 
-    else:
-        if "catalog" in targets:
-            drop_scope_query = f"DROP SCOPE `{cfg.bucket}`.`{DEFAULT_CATALOG_SCOPE}` IF EXISTS;"
+        else:
+            drop_scope_query = f"DROP SCOPE `{bucket}`.`{DEFAULT_AUDIT_SCOPE}` IF EXISTS;"
             res, err = execute_query(cluster, drop_scope_query)
             for r in res.rows():
                 logger.debug(r)
             if err is not None:
                 all_errs.append(err)
 
-        if "activity" in targets:
-            drop_scope_query = f"DROP SCOPE `{cfg.bucket}`.`{DEFAULT_ACTIVITY_SCOPE}` IF EXISTS;"
-            res, err = execute_query(cluster, drop_scope_query)
-            for r in res.rows():
-                logger.debug(r)
-            if err is not None:
-                all_errs.append(err)
-
-        if len(all_errs) > 0:
-            logger.error(all_errs)
+                if len(all_errs) > 0:
+                    logger.error(all_errs)
 
     return len(all_errs)
 
 
-@logging_command(logger)
 def cmd_clean(
-    cfg: Config = None,
-    *,
     is_local: bool,
     is_db: bool,
+    bucket: str,
+    cluster: couchbase.cluster.Cluster,
     catalog_ids: tuple[str],
     kind: list[typing.Literal["tool", "prompt"]],
-    targets: list[Literal["catalog", "activity"]],
+    ctx: Context = None,
+    type_metadata: Literal["catalog", "activity", "all"] = "all",
+    date: str = None,
 ):
-    if cfg is None:
-        cfg = Config()
-
     if is_local:
-        clean_local(cfg, targets)
-        click_extra.secho("Local FS catalog/metadata has been deleted!", fg="green")
+        clean_local(ctx, type_metadata, date)
+        click.secho("Local catalog/metadata/logs has been deleted!", fg="green")
 
     if is_db:
-        num_errs = clean_db(cfg, catalog_ids, kind, targets)
+        num_errs = clean_db(ctx, bucket, cluster, catalog_ids, kind, type_metadata, date)
         if num_errs > 0:
-            raise ValueError("Failed to cleanup DB catalog/metadata!")
+            raise ValueError("Failed to cleanup db catalog/metadata!")
         else:
-            click_extra.secho("Database catalog/metadata has been deleted!", fg="green")
+            click.secho("Database catalog/metadata/logs has been deleted!", fg="green")
 
 
 # Note: flask is an optional dependency.
@@ -144,7 +188,7 @@ if importlib.util.find_spec("flask") is not None:
         if True:  # TODO: Should check REST args on whether to clean local catalog.
             clean_local(ctx, None)
 
-        # if False:  # TODO: Should check REST args on whether to clean db.
-        #     clean_db(ctx, "TODO", None)
+        if False:  # TODO: Should check REST args on whether to clean db.
+            clean_db(ctx, "TODO", None)
 
         return "OK"  # TODO.
--- libs/agentc_cli/agentc_cli/cmds/util.py
+++ libs/agentc_cli/agentc_cli/cmds/util.py
@@ -1,16 +1,16 @@
-import click_extra
-import couchbase.exceptions
+import click
+import couchbase.cluster
 import datetime
-import functools
 import git
 import logging
 import os
 import pathlib
 import pydantic
 import re
-import traceback
+import shutil
 import typing
 
+from ..models.context import Context
 from agentc_core.catalog import CatalogChain
 from agentc_core.catalog import CatalogDB
 from agentc_core.catalog import CatalogMem
@@ -18,43 +18,30 @@ from agentc_core.catalog import __version__ as CATALOG_SCHEMA_VERSION
 from agentc_core.catalog.index import MetaVersion
 from agentc_core.catalog.index import index_catalog
 from agentc_core.catalog.version import lib_version
-from agentc_core.config import Config
+from agentc_core.defaults import DEFAULT_CATALOG_NAME
 from agentc_core.defaults import DEFAULT_MAX_ERRS
-from agentc_core.defaults import DEFAULT_PROMPT_CATALOG_FILE
 from agentc_core.defaults import DEFAULT_SCAN_DIRECTORY_OPTS
-from agentc_core.defaults import DEFAULT_TOOL_CATALOG_FILE
+from agentc_core.learned.embedding import EmbeddingModel
 from agentc_core.version import VersionDescriptor
 
-logger = logging.getLogger(__name__)
-
 # The following are used for colorizing output.
-CATALOG_KINDS = ["prompt", "tool"]
 LEVEL_COLORS = {"good": "green", "warn": "yellow", "error": "red"}
 KIND_COLORS = {"tool": "bright_magenta", "prompt": "blue", "log": "cyan"}
 try:
     DASHES = "-" * os.get_terminal_size().columns
-except OSError as _e:
-    logger.debug(f"Unable to retrieve the terminal screen size. Swallowing exception {str(_e)}.")
+except OSError:
     # We might run into this error while running in a debugger.
     DASHES = "-" * 80
 
+logger = logging.getLogger(__name__)
 
-def logging_command(parent_logger: logging.Logger):
-    # This decorator is used to catch unrecoverable errors from commands (mainly for testing purposes).
-    def decorator(func):
-        @functools.wraps(func)
-        def new_func(*args, **kwargs):
-            parent_logger.debug(f"Running command {func.__name__}.")
-            try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                parent_logger.debug(f"Command {func.__name__} failed with exception: {str(e)}")
-                parent_logger.debug(traceback.format_exception(e))
-                raise e
 
-        return new_func
+def init_local(ctx: Context):
+    # Init directories.
+    os.makedirs(ctx.catalog, exist_ok=True)
+    os.makedirs(ctx.activity, exist_ok=True)
 
-    return decorator
+    # (Note: the version checking logic has been moved into index).
 
 
 def load_repository(top_dir: pathlib.Path = None):
@@ -101,53 +88,36 @@ def load_repository(top_dir: pathlib.Path = None):
 
 
 def get_catalog(
-    cfg: Config,
+    catalog_path: str,
+    bucket: str,
+    cluster: couchbase.cluster.Cluster,
+    force_db: bool,
     include_dirty: bool,
     kind: typing.Literal["tool", "prompt"],
-    force: typing.Literal["local", "db", "chain"] = None,
-    printer: typing.Callable[[str], None] = None,
 ):
-    # By default, we'll print using click_extra.
-    if printer is None:
-        printer = click_extra.secho
-
     # We have three options: (1) db catalog, (2) local catalog, or (3) both.
     repo, get_path_version = load_repository(pathlib.Path(os.getcwd()))
-    if kind == "tool":
-        catalog_file = cfg.CatalogPath() / DEFAULT_TOOL_CATALOG_FILE
-    elif kind == "prompt":
-        catalog_file = cfg.CatalogPath() / DEFAULT_PROMPT_CATALOG_FILE
-    else:
-        raise ValueError(f"Unknown catalog kind: {kind}")
+    catalog_file = pathlib.Path(catalog_path) / (kind + DEFAULT_CATALOG_NAME)
     db_catalog, local_catalog = None, None
-
-    # Path #1: Search our DB catalog.
-    try:
-        cluster = cfg.Cluster()
-    except (ValueError, couchbase.exceptions.CouchbaseException) as e:
-        if force == "db":
-            raise e
-        else:
-            logger.debug(f"Unable to initialize DB cluster. Swallowing exception: {str(e)}")
-            cluster = None
-    if force == "db" and (cfg.bucket is None or cluster is None):
-        raise ValueError("Must provide a bucket and cluster to search the DB catalog.")
-    if cfg.bucket is not None and cluster is not None:
+    if bucket is not None and cluster is not None:
+        # Path #1: Search our DB catalog.
         try:
-            embedding_model = cfg.EmbeddingModel("NAME", "LOCAL", "DB")
-            db_catalog = CatalogDB(cluster=cluster, bucket=cfg.bucket, kind=kind, embedding_model=embedding_model)
+            embedding_model = EmbeddingModel(
+                catalog_path=pathlib.Path(catalog_path),
+                cb_bucket=bucket,
+                cb_cluster=cluster,
+            )
+            db_catalog = CatalogDB(cluster=cluster, bucket=bucket, kind=kind, embedding_model=embedding_model)
         except pydantic.ValidationError as e:
-            if force == "db":
+            if force_db:
                 raise e
-            else:
-                logger.debug(f"Unable to initialize DB catalog. Swallowing exception: {str(e)}")
-
-    # Path #2: Search our local catalog.
-    if force == "local" and not catalog_file.exists():
-        raise ValueError(f"Could not find local catalog at {catalog_file}.")
-    if catalog_file.exists():
-        embedding_model = cfg.EmbeddingModel("NAME", "LOCAL")
-        local_catalog = CatalogMem(catalog_file=catalog_file, embedding_model=embedding_model)
+    if catalog_file.exists() and not force_db:
+        # Path #2: Search our local catalog.
+        catalog_path = pathlib.Path(catalog_path) / (kind + DEFAULT_CATALOG_NAME)
+        embedding_model = EmbeddingModel(
+            catalog_path=pathlib.Path(catalog_path),
+        )
+        local_catalog = CatalogMem(catalog_path=catalog_file, embedding_model=embedding_model)
 
         if include_dirty and repo and repo.is_dirty():
             # The repo and any dirty files do not have real commit id's, so use "DIRTY".
@@ -159,17 +129,15 @@ def get_catalog(
             # Create a CatalogMem on-the-fly that incorporates the dirty
             # source file items which we'll use instead of the local catalog file.
             meta_version = MetaVersion(schema_version=CATALOG_SCHEMA_VERSION, library_version=lib_version())
-
-            # If we are in debug mode, we'll print the dirty files.
-            indexer_printer = printer
             if logger.getEffectiveLevel() == logging.DEBUG:
 
                 def logging_printer(content: str, *args, **kwargs):
                     logger.debug(content)
-                    printer(content, *args, **kwargs)
+                    click.secho(content, *args, **kwargs)
 
-                indexer_printer = logging_printer
-
+                printer = logging_printer
+            else:
+                printer = click.secho
             local_catalog = index_catalog(
                 embedding_model,
                 meta_version,
@@ -179,33 +147,37 @@ def get_catalog(
                 catalog_file,
                 source_dirs,
                 scan_directory_opts=DEFAULT_SCAN_DIRECTORY_OPTS,
-                printer=indexer_printer,
+                printer=printer,
                 print_progress=True,
                 max_errs=DEFAULT_MAX_ERRS,
             )
-            printer("\n", nl=False)
+            click.secho("\n", nl=False)
+    # Query the catalog for a list of results.
+    if db_catalog and local_catalog:
+        # Option #3: Chain the local and db catalogs.
+        click.secho("Searching both local and db catalogs.")
+        catalog = CatalogChain(local_catalog, db_catalog)
+    elif db_catalog:
+        click.secho("Searching db catalog.")
+        catalog = db_catalog
+    elif local_catalog:
+        click.secho("Searching local catalog.")
+        catalog = local_catalog
+    else:
+        raise ValueError("No catalog found!")
+    return catalog
 
-    # Deliver our catalog.
-    if force == "local" and local_catalog:
-        printer("Searching local catalog.")
-        return local_catalog
-    elif force == "db" and db_catalog:
-        printer("Searching db catalog.")
-        return db_catalog
-    elif force == "chain" and db_catalog and local_catalog:
-        printer("Searching both local and db catalogs.")
-        return CatalogChain(local_catalog, db_catalog)
-    elif force is None:
-        if local_catalog:
-            printer("Searching local catalog.")
-            return local_catalog
-        elif db_catalog:
-            printer("Searching db catalog.")
-            return db_catalog
-        elif local_catalog and db_catalog:
-            printer("Searching both local and db catalogs.")
-            return CatalogChain(local_catalog, db_catalog)
-    raise ValueError("No catalog found!")
+
+def remove_directory(folder: str):
+    if not folder or not os.path.exists(folder):
+        return
+
+    folder_path = pathlib.Path(folder)
+
+    if folder_path.is_file():
+        os.remove(folder_path.absolute())
+    elif folder_path.is_dir():
+        shutil.rmtree(folder_path.absolute())
 
 
 # TODO: One use case is a user's repo (like agent-catalog-example) might
