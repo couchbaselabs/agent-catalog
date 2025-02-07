f11c9d2ff46b2dc825ec229c1805444bc0b93150
ThejasNU <thejas.nu@couchbase.com>
2025-02-07 10:48:33 +0530

    update comments

--- libs/agentc_core/agentc_core/learned/model.py
+++ libs/agentc_core/agentc_core/learned/model.py
@@ -8,11 +8,11 @@ class EmbeddingModel(pydantic.BaseModel):
     )
     name: str = pydantic.Field(
         description="The name of the embedding model being used.",
-        examples=["all-MiniLM-L12-v2", "https://12fs345d.apps.cloud.couchbase.com"],
+        examples=["all-MiniLM-L12-v2", "intfloat/e5-mistral-7b-instruct"],
     )
     base_url: typing.Optional[str] = pydantic.Field(
         description="The base URL of the embedding model."
-        "This field must be specified is using a non-SentenceTransformers-based model.",
+        "This field must be specified if using a non-SentenceTransformers-based model.",
         examples=["https://12fs345d.apps.cloud.couchbase.com"],
         default=None,
     )
