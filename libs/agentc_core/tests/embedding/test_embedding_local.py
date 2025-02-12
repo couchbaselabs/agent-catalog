5a51d6f9935a6337588913a25df218a2abdbbbb0
ThejasNU <thejas.nu@couchbase.com>
2025-02-12 15:44:22 +0530

    fix core tests

--- libs/agentc_core/tests/embedding/test_embedding_local.py
+++ libs/agentc_core/tests/embedding/test_embedding_local.py
@@ -1,11 +1,19 @@
 import pytest
+import sentence_transformers
 
 from agentc_core.defaults import DEFAULT_EMBEDDING_MODEL
+from agentc_core.defaults import DEFAULT_MODEL_CACHE_FOLDER
 from agentc_core.learned.embedding import EmbeddingModel
 
 
 @pytest.mark.smoke
 def test_embedding_local_default():
+    # download the model
+    sentence_transformers.SentenceTransformer(
+        DEFAULT_EMBEDDING_MODEL, cache_folder=DEFAULT_MODEL_CACHE_FOLDER, local_files_only=False
+    )
+
+    # execute the model
     embedding_model = EmbeddingModel(
         embedding_model_name=DEFAULT_EMBEDDING_MODEL,
     )
@@ -16,6 +24,12 @@ def test_embedding_local_default():
 
 @pytest.mark.smoke
 def test_embedding_local_pretrained():
+    # download the model
+    sentence_transformers.SentenceTransformer(
+        "paraphrase-albert-small-v2", cache_folder=DEFAULT_MODEL_CACHE_FOLDER, local_files_only=False
+    )
+
+    # execute the model
     embedding_model = EmbeddingModel(
         embedding_model_name="paraphrase-albert-small-v2",
     )
